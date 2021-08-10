269aa528c03d35198426500f5087c5a2bea5da7d
imlonghao <git@imlonghao.com>
2021-08-10 09:53:28 +0000

    cachix: add imlonghao

---
+++ profiles/cachix/imlonghao.nix
@@ -0,0 +1,10 @@
+{
+  nix = {
+    binaryCaches = [
+      "https://imlonghao.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "imlonghao.cachix.org-1:KGQ7+R1BXo2NsoeAxKLPfGAiHz5ofCmZO4hih7u/2Q8="
+    ];
+  };
+}
