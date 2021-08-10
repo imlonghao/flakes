f4affded652b070da18c5e014dd6eb218ff76627
imlonghao <git@imlonghao.com>
2021-08-10 23:00:03 +0800

    hertz: add bird router id

--- hosts/hertzdefalkenstein1.nix
+++ hosts/hertzdefalkenstein1.nix
@@ -39,48 +39,49 @@ lnUteXuaejULw== openpgp:0x1E1A60D7"
   services.bird2 = {
     enable = true;
     config = ''
+      router id 100.64.88.58;
       protocol direct {
-          ipv4;
-          ipv6;
+        ipv4;
+        ipv6;
       }
       protocol device {
-          scan time 10;
+        scan time 10;
       }
       protocol kernel {
-          scan time 10;
-          graceful restart on;
-          ipv4 {
-              import none;
-              export all;
-          };
+        scan time 10;
+        graceful restart on;
+        ipv4 {
+          import none;
+          export all;
+        };
       }
       protocol kernel {
-          scan time 10;
-          graceful restart on;
-          ipv6 {
-              import none;
-              export where net != ::/0;
-          };
+        scan time 10;
+        graceful restart on;
+        ipv6 {
+          import none;
+          export where net != ::/0;
+        };
       }
       protocol bgp AS135395v6 {
-          neighbor 2a0f:9400:7a00::1 as 135395;
-          source address 2a0f:9400:7a00:1111:deb4::;
-          local as 133846;
-          graceful restart on;
-          ipv6 {
-              import none;
-              export where net = 2602:feda:1bf::/48 || net = 2a09:b280:ff82::/48;
-          };
+        neighbor 2a0f:9400:7a00::1 as 135395;
+        source address 2a0f:9400:7a00:1111:deb4::;
+        local as 133846;
+        graceful restart on;
+        ipv6 {
+          import none;
+          export where net = 2602:feda:1bf::/48;
+        };
       }
       protocol bgp HZIXv6 {
-          neighbor 2a0f:9400:7a00:3333:1111::1 as 64555;
-          source address 2a0f:9400:7a00:3333:9e62::1;
-          local as 133846;
-          graceful restart on;
-          ipv6 {
-              import all;
-              export where net = 2602:feda:1bf::/48 || net = 2a09:b280:ff82::/48;
-          };
+        neighbor 2a0f:9400:7a00:3333:1111::1 as 64555;
+        source address 2a0f:9400:7a00:3333:9e62::1;
+        local as 133846;
+        graceful restart on;
+        ipv6 {
+          import all;
+          export where net = 2602:feda:1bf::/48;
+        };
       }
     '';
   };
