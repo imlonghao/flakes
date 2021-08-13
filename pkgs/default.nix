9e47599c9e19e0d6b87b1cce726dedddef43b28e
imlonghao <git@imlonghao.com>
2021-08-14 01:39:49 +0800

    pkgs(babeld): add to default.nix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,5 +2,6 @@ final: prev: {
   # keep sources this first
   sources = prev.callPackage (import ./_sources/generated.nix) { };
   # then, call packages with `final.callPackage`
+  babeld = prev.callPackage ./babeld { };
   rait = prev.callPackage ./rait { };
 }
