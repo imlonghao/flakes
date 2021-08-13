3a06cd717c01db16c3df3be47300197be21fddcb
imlonghao <git@imlonghao.com>
2021-08-13 21:39:05 +0800

    secrets(rait): rekeys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,8 @@
 let
   me = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKv5fgCyrSdHw1z4Yvdi28fLs413vLFYk5sYyfC1YHJz imlonghao@imlonghao";
   deployer = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGPiPq3zWmsO7dEJS/xR8+YW2eEFpPoR7ybtXwh0kC3S imlonghao@hetzner-fi-helsinki-1";
-  allKeys = [ me deployer ];
+  hertzdefalkenstein1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGxXK+vxVnjNTxZU+MzK7jiJJJ0lcq4uOz8Oe88KBiOy";
+  allKeys = [ me deployer hertzdefalkenstein1 ];
 in
 {
   "rait/rait.sh".publicKeys = allKeys;
