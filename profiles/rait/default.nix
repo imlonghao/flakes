c75dbda10be23c07078f609be5cdf195bd7f5fcd
imlonghao <git@imlonghao.com>
2021-08-13 21:05:09 +0800

    secrets(rait): fix age error

--- profiles/rait/default.nix
+++ profiles/rait/default.nix
@@ -6,10 +6,6 @@
     mode = "0500";
   };
   age.secrets."rait.conf".file = "${self}/secrets/rait/${config.networking.hostName}.conf";
-  environment.etc."rait/rait.conf" = {
-    source = age.secrets."rait.conf".path;
-    mode = "0400";
-  };
   services.rait = {
     enable = true;
     path = age.secrets."rait.sh".path;
