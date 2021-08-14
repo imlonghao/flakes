acfebacd13f591f14829109e5422350abadc28b3
imlonghao <git@imlonghao.com>
2021-08-14 11:20:37 +0800

    Revert "pkgs(babeld): add to default.nix"
    This reverts commit 9e47599c9e19e0d6b87b1cce726dedddef43b28e.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,6 +2,5 @@ final: prev: {
   # keep sources this first
   sources = prev.callPackage (import ./_sources/generated.nix) { };
   # then, call packages with `final.callPackage`
-  babeld = prev.callPackage ./babeld { };
   rait = prev.callPackage ./rait { };
 }
