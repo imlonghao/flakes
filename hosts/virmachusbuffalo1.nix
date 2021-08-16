3025f958da0c7439d586428e7a08030cc032d30e
imlonghao <git@imlonghao.com>
2021-08-17 00:25:35 +0800

    hosts(virmach): remove conflict

--- hosts/virmachusbuffalo1.nix
+++ hosts/virmachusbuffalo1.nix
@@ -12,7 +12,6 @@
   networking.dhcpcd.allowInterfaces = [ "ens3" ];
 
   # hardware-configuration.nix
-  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
   boot.loader.grub.device = "/dev/vda";
   boot.initrd.kernelModules = [ "nvme" ];
   fileSystems."/" = { device = "/dev/vda1"; fsType = "ext4"; };
