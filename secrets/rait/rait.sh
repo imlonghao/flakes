63a058bc7e5dfedbad8bfbc593780a7f4b6c3f40
imlonghao <git@imlonghao.com>
2021-08-24 22:30:26 +0800

    secrets(agenix): resign

--- secrets/rait/rait.sh
+++ secrets/rait/rait.sh
@@ -1,16 +1,16 @@
 age-encryption.org/v1
--> ssh-ed25519 g44T1w cv0WD++YnOzZnEEUbFUQrjk5DV7+2gtJOrHdmZAjwQk
-2rFAf8S1Ouh9y41lgNW9xilzeZvXLMx/mgkR4cqZ/ck
--> ssh-ed25519 sVpmpg PZUnE4Hg88ayz1gDHV84RQ7zzIEpJ7WBPudWooH+Y2I
-xmvjptQiF6bDSry9mvXsuhVz06LS4f9EaoJvbPHGnEI
--> ssh-ed25519 j4VEsA bfd2jsvsTopVGi4dc97HSCOEvh3SwvpbjMOAhxEKu1s
-erB7BnwWiUfawzgdQ1VHswWFw+60ej8a6CTQLrw1rsI
--> ssh-ed25519 1rO35Q ByPP+u8fe4RuEWCPCXX6vFxKbiDVat0PuZQQdMYk0RQ
-1lx6RoGulzrJ1T0B1RfKqp1SQDchWZnD9mCWbqa3bhk
--> dr3-grease s@ p>Q+cl [le7QeZ J}1P1
-osl9eRHpguS8YM41Vn1tbo94w1cjyGMbtYgQ6PkoqtONZjwO3RYs6oagfmsMgWG+
-VnsBQI1TZQ8vRDM6pYtF+rzlpRs5
---- tyumrP5gQqq/8L1oPhYzAnOY4sabFCa0TBT7S9qSbb4
-»,›÷g•Fc²ÇÓÂXŒT‡Û~¿–¾ðIÐÍ“æÊ·x½5–FÁ9Æ9-j»tIx~“#4Ì{Xë5ô‡»Î”›P‚ƒSkÒ	/,°S•Úä1xX'‰™Vô?™ŸÐ½å}7‘Sr/ñ`î
-–ÕðÑü@’&Tìã?ÕóÑé˜¡þb§vr¼nˆ!@¼\B™VÛ†AU|”?È’9:z7êhŽ=b–hŠpX+8¤:Š­Õ%•L|7©1¨Ë[¿nX§tíœ$K:ShÀò‡²¦´Kõ‹ØÒ
-Ï$G ~W[øÚÒØØOüp‹dß‘U"º5z²\*ií5Œ“ö>Ÿ”|ÝÚiýqôa¼ZxÔáÈrMÏ2P<Þã¾Xáì"šZ÷ÚqM@jä¹È¢-NôàŽÓwÑl$Ùˆ_è»YX½4<ú²
+-> ssh-ed25519 g44T1w GV/3NCuMJYSmXsA8HyED7MNlwDktXp6GSK64rkpYkx8
+ACyN5CNW/0Eg/SPSF84DBrNS9wtICTmSm57O1JfJhEs
+-> ssh-ed25519 sVpmpg D+OArTMPjgWdUuGpPWpmfYGk9RfShGFREogsql+KaSo
+oacc3Uod+WIZplidfnfO21VfR3ayvOnSi/HlwWZTMTU
+-> ssh-ed25519 j4VEsA dyIh3xp8EimRIRe8xYQBa5DZXM3gwfI26uTvCoW5VE8
+fNuBhoYnynSY63SjvQYdeAuj8+CaFNq5U/8o27WOs8A
+-> ssh-ed25519 1rO35Q eQqPLhPqBR5V0j9h2xuR034xgjEK8o8VxdzNpgaOXkI
+6to5ftMB8u/iaIJ+lnFtVYj4MB4ysHlBrGMFbKM1fxI
+-> ssh-ed25519 mrL2jw VZnxlmDutb9fvYykinSznlQFgX7X6Y+tfpzEAt6r6SY
+Kjng3xCV1ATJeZ2Dr2gGBDabGSUp1en7G96Ue1H/Z58
+-> vSu-grease /HUF.gF #^@ <a7sQ
+Z812qullWJ/1qA6ValafHw
+--- GOoMKn6Dll+7dtOuRrpVccd7iIpWeh7sb8mv7vpbBkw
+t¶4ÄGp´Iå»\ZÚª˜7tÂ·9BÕr˜¸d`¦Ü‹cü;&9ý*#`Ì.ÐDŒ‘ôÏa-Q²tPN™bÒ*_»rëT†Ä>÷”dW9”AÐ¤¦?³)õn1Æõ¼ k}%—¯£ÆÆlO?2Qú~í:,)bÐÔy¦«@÷çÒ‘uwŽÇé]ÆÌ:X35–d¨¢£¼m*¼Å¨p&ö…ÞµÐºŸÄÁàÉ-ÇKÊÒ5«åNö¤‘<œ
+C…lIôÓ‰íF]Ï;¸Qö·Þ€´ÌKÁ*?ôúˆø:7ûñóUÎU€ÑPÖW:Ou!6óva'Ò=2ßô¹ŽW­î	Éq¢¶Å¿–i-<DÐ½ ü8õçÔæ¬t¼üIãý¾Gødo£Å’°ú¡peà¹°É¹Ûa9©á<óÏ¸“ì«-áã^·xBt•Ÿ}¿
