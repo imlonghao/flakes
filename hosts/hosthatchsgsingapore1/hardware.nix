e2ca9562c746c34d83417eb55bdbb7fef4db5109
imlonghao <git@imlonghao.com>
2021-08-24 21:54:46 +0800

    host(hosthatch): format

--- hosts/hosthatchsgsingapore1/hardware.nix
+++ hosts/hosthatchsgsingapore1/hardware.nix
@@ -14,26 +14,21 @@ in
 
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "sr_mod" "virtio_blk" ];
 
-  fileSystems."/boot" =
-    {
-      device = "/dev/disk/by-uuid/44faab9b-aabd-40a2-a10d-02d7a4a90ca1";
-      fsType = "btrfs";
-      options = [ "subvol=@boot" ] ++ mountOptions;
-    };
-
-  fileSystems."/nix" =
-    {
-      device = "/dev/disk/by-uuid/44faab9b-aabd-40a2-a10d-02d7a4a90ca1";
-      fsType = "btrfs";
-      options = [ "subvol=@nix" ] ++ mountOptions;
-    };
-
-  fileSystems."/persist" =
-    {
-      device = "/dev/disk/by-uuid/44faab9b-aabd-40a2-a10d-02d7a4a90ca1";
-      fsType = "btrfs";
-      options = [ "subvol=@persist" ] ++ mountOptions;
-      neededForBoot = true;
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/44faab9b-aabd-40a2-a10d-02d7a4a90ca1";
+    fsType = "btrfs";
+    options = [ "subvol=@boot" ] ++ mountOptions;
+  };
+  fileSystems."/nix" = {
+    device = "/dev/disk/by-uuid/44faab9b-aabd-40a2-a10d-02d7a4a90ca1";
+    fsType = "btrfs";
+    options = [ "subvol=@nix" ] ++ mountOptions;
+  };
+  fileSystems."/persist" = {
+    device = "/dev/disk/by-uuid/44faab9b-aabd-40a2-a10d-02d7a4a90ca1";
+    fsType = "btrfs";
+    options = [ "subvol=@persist" ] ++ mountOptions;
+    neededForBoot = true;
+  };
 
 }
