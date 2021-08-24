7c8e0f22ad4b335df50aa7ba09d2e61fb3991159
imlonghao <git@imlonghao.com>
2021-08-24 22:16:59 +0800

    secrets(rait): add hosthatch

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,9 +4,10 @@ let
 
   hertzdefalkenstein1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGxXK+vxVnjNTxZU+MzK7jiJJJ0lcq4uOz8Oe88KBiOy";
   virmachusbuffalo1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIa8dYtwsbAmVoohOb5VzpWoOGh/pwipKa8beVDvy6FA";
+  hosthatchsgsingapore1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBID8nCh4rMu4rhADLBjHR4zUvNWKF7898FHzkrBKY3C";
 
   users = [ me deployer ];
-  hosts = [ hertzdefalkenstein1 virmachusbuffalo1 ];
+  hosts = [ hertzdefalkenstein1 virmachusbuffalo1 hosthatchsgsingapore1 ];
 
   allKeys = users ++ hosts;
 in
@@ -14,4 +15,5 @@ in
   "rait/rait.sh".publicKeys = allKeys;
   "rait/hertzdefalkenstein1.conf".publicKeys = [ me hertzdefalkenstein1 ];
   "rait/virmachusbuffalo1.conf".publicKeys = [ me virmachusbuffalo1 ];
+  "rait/hosthatchsgsingapore1.conf".publicKeys = [ me hosthatchsgsingapore1 ];
 }
