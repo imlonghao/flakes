027e981cf66aa31ea4116125362e93b40d85f218
imlonghao <git@imlonghao.com>
2021-08-28 11:18:30 +0800

    host(uovz): add rait

--- hosts/uovzcnhongkong1/default.nix
+++ hosts/uovzcnhongkong1/default.nix
@@ -5,6 +5,7 @@
     ./hardware.nix
     profiles.mycore
     profiles.users.root
+    profiles.rait
   ];
 
   boot.loader.grub.device = "/dev/vda";
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,9 +5,10 @@ let
   hertzdefalkenstein1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGxXK+vxVnjNTxZU+MzK7jiJJJ0lcq4uOz8Oe88KBiOy";
   virmachusbuffalo1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIa8dYtwsbAmVoohOb5VzpWoOGh/pwipKa8beVDvy6FA";
   hosthatchsgsingapore1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBID8nCh4rMu4rhADLBjHR4zUvNWKF7898FHzkrBKY3C";
+  uovzcnhongkong1 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFLUpy8RTdbWL3SeKpladeChdgCZz2rIVrRgr2POqc+j";
 
   users = [ me deployer ];
-  hosts = [ hertzdefalkenstein1 virmachusbuffalo1 hosthatchsgsingapore1 ];
+  hosts = [ hertzdefalkenstein1 virmachusbuffalo1 hosthatchsgsingapore1 uovzcnhongkong1 ];
 
   allKeys = users ++ hosts;
 in
@@ -16,4 +17,5 @@ in
   "rait/hertzdefalkenstein1.conf".publicKeys = [ me hertzdefalkenstein1 ];
   "rait/virmachusbuffalo1.conf".publicKeys = [ me virmachusbuffalo1 ];
   "rait/hosthatchsgsingapore1.conf".publicKeys = [ me hosthatchsgsingapore1 ];
+  "rait/uovzcnhongkong1.conf".publicKeys = [ me uovzcnhongkong1 ];
 }
