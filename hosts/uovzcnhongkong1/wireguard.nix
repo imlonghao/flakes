75d22a5b44f3293ddde4b06de3447abe5070bddc
imlonghao <git@imlonghao.com>
2021-09-06 00:14:46 +0800

    hosts(uovz): peer AS4242421123

--- hosts/uovzcnhongkong1/wireguard.nix
+++ hosts/uovzcnhongkong1/wireguard.nix
@@ -4,6 +4,19 @@ let
 in
 {
   networking.wireguard.interfaces = {
+    wg1123 = {
+      ips = [ "fe80::1123/64" ];
+      privateKey = wgPrivKey;
+      listenPort = 21123;
+      allowedIPsAsRoutes = false;
+      peers = [
+        {
+          endpoint = "hkg.ccp.ovh:21888";
+          publicKey = "bLysMVqew2hlvJVS17hp+qaURzZuLERLgfSbvK/mgUU=";
+          allowedIPs = [ "10.0.0.0/8" "172.20.0.0/14" "172.31.0.0/16" "fe80::/64" "fd00::/8" ];
+        }
+      ];
+    };
     wg1817 = {
       ips = [ "fe80::1888/64" ];
       postSetup = "${pkgs.iproute2}/bin/ip addr add 172.22.68.0/32 peer 172.22.77.33/32 dev wg1817";
