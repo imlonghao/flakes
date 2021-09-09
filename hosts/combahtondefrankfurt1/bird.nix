2b523b73afbf540b070ebc252efb4398f512a880
imlonghao <git@imlonghao.com>
2021-09-09 21:47:38 +0800

    Revert "hosts(combahton): peer AS4242422188"
    This reverts commit 06cccce2cfcfa8923b25aa5455ef29e8a4c1b69f.

--- hosts/combahtondefrankfurt1/bird.nix
+++ hosts/combahtondefrankfurt1/bird.nix
@@ -100,9 +100,6 @@
       protocol bgp AS4242421588 from dnpeers {
         neighbor fe80::1588 % 'wg1588' as 4242421588;
       }
-      protocol bgp AS4242422188 from dnpeers {
-        neighbor fe80::2188 % 'wg2188' as 4242422188;
-      }
       protocol bgp AS4242422980 from dnpeers {
         neighbor fe80::2980 % 'wg2980' as 4242422980;
       }
