3cf02111053e045b1ade0e219d8a887901b8b333
imlonghao <git@imlonghao.com>
2021-09-16 21:28:19 +0800

    profiles(k3s): fix age path

--- profiles/k3s/default.nix
+++ profiles/k3s/default.nix
@@ -5,7 +5,7 @@
   services.k3s = {
     enable = true;
     role = "agent";
-    tokenFile = age.secrets."k3s.token".path;
+    tokenFile = "/run/secrets/rait.sh";
     serverAddr = "https://100.64.88.62:6443";
   };
 }
