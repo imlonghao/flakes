bc3e90b2c39af4130114e3b36acc21a48047f5e6
imlonghao <git@imlonghao.com>
2021-09-16 21:51:35 +0800

    profiles(k3s): fix tokenFile

--- profiles/k3s/default.nix
+++ profiles/k3s/default.nix
@@ -5,7 +5,8 @@
   services.k3s = {
     enable = true;
     role = "agent";
-    tokenFile = "/run/secrets/rait.sh";
-    serverAddr = "https://100.64.88.62:6443";
+    tokenFile = "/run/secrets/k3s.token;
+    serverAddr = " https://100.64.88.62:6443 ";
   };
 }
+
