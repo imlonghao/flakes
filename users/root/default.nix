1fc7154e912ab85e96114966bd78c9fc35d34733
imlonghao <git@imlonghao.com>
2021-10-12 02:18:00 +0000

    user(root): make default shell to fish

--- users/root/default.nix
+++ users/root/default.nix
@@ -1,4 +1,4 @@
-{ self, ... }:
+{ pkgs, self, ... }:
 {
   users.users.root = {
     hashedPassword = builtins.readFile "${self}/secrets/root";
@@ -10,5 +10,6 @@ lnUteXuaejULw== openpgp:0x1E1A60D7"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKv5fgCyrSdHw1z4Yvdi28fLs413vLFYk5sYyfC1YHJz imlonghao@imlonghao"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGPiPq3zWmsO7dEJS/xR8+YW2eEFpPoR7ybtXwh0kC3S imlonghao@hetzner-fi-helsinki-1"
     ];
+    shell = pkgs.fish;
   };
 }
