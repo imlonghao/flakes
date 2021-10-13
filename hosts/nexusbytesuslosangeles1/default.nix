548fe7e9bfd24ab650d8182876d47e430be91abe
imlonghao <git@imlonghao.com>
2021-10-13 03:28:05 +0000

    host(nexus): make sure gateway have route

--- hosts/nexusbytesuslosangeles1/default.nix
+++ hosts/nexusbytesuslosangeles1/default.nix
@@ -19,12 +19,20 @@
     nameservers = [ "8.8.8.8" "1.1.1.1" ];
     defaultGateway6 = "2602:fed2:7106::1";
     interfaces = {
-      ens3.ipv6.addresses = [
-        {
-          address = "2602:fed2:7106:271b::1";
-          prefixLength = 64;
-        }
-      ];
+      ens3.ipv6 = {
+        addresses = [
+          {
+            address = "2602:fed2:7106:271b::1";
+            prefixLength = 64;
+          }
+        ];
+        routes = [
+          {
+            "address": "2602:fed2:7106::1",
+            "prefixLength": 128
+          }
+        ];
+      };
     };
   };
 
