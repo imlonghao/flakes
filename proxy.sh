9826f4c22808030bddfb7c316e90a3a0d31fe3fc
imlonghao <git@imlonghao.com>
2021-11-30 23:11:57 +0800

    add proxy.sh for local

---
+++ proxy.sh
@@ -0,0 +1,5 @@
+export CURL_NIX_FLAGS="-x http://127.0.0.1:8118/"
+export HTTP_PROXY=http://127.0.0.1:8118/
+export HTTPS_PROXY=http://127.0.0.1:8118/
+export http_proxy=http://127.0.0.1:8118/
+export https_proxy=http://127.0.0.1:8118/
