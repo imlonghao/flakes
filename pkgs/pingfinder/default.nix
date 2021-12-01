32406f317136647121848695c8ca251be024a9c1
imlonghao <git@imlonghao.com>
2021-12-01 15:49:15 +0800

    pingfinder: debug

--- pkgs/pingfinder/default.nix
+++ pkgs/pingfinder/default.nix
@@ -30,7 +30,7 @@ stdenv.mkDerivation rec {
     ls -al "$out/bin/"
     [[ -f "$out/bin/pingfinder" ]] || echo "fail 1"
     [[ -x "$out/bin/pingfinder" ]] || echo "fail 2"
-    env
+    $out/bin/pingfinder
     wrapProgram "$out/bin/pingfinder" --set PATH "${lib.makeBinPath [
       coreutils
       curl
