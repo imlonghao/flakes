6e40abba9f225a66f50980061934a3a6b540d399
imlonghao <git@imlonghao.com>
2021-12-02 22:36:13 +0800

    profile(k3s): move to sops

--- profiles/k3s/default.nix
+++ profiles/k3s/default.nix
@@ -1,11 +1,11 @@
-{ age, pkgs, self, ... }:
+{ config, pkgs, self, sops, ... }:
 
 {
-  age.secrets."k3s.token".file = "${self}/secrets/k3s.token";
+  sops.secrets."k3s.token".sopsFile = "${self}/secrets/k3s.yml";
   services.k3s = {
     enable = true;
     role = "agent";
-    tokenFile = "/run/secrets/k3s.token";
+    tokenFile = config.sops.secrets."k3s.token".path;
     serverAddr = "https://100.64.88.62:6443";
   };
   services.k3s-no-ctstate-invalid.enable = true;
