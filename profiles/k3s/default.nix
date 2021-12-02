98ec5e958d6dc2d3a2ee5230b8f6549d02acef47
imlonghao <git@imlonghao.com>
2021-12-02 22:59:19 +0800

    profile(k3s): make the name beauty

--- profiles/k3s/default.nix
+++ profiles/k3s/default.nix
@@ -1,11 +1,11 @@
 { config, pkgs, self, sops, ... }:
 
 {
-  sops.secrets.k3stoken.sopsFile = "${self}/secrets/k3s.yml";
+  sops.secrets."k3s.token".sopsFile = "${self}/secrets/k3s.yml";
   services.k3s = {
     enable = true;
     role = "agent";
-    tokenFile = config.sops.secrets.k3stoken.path;
+    tokenFile = config.sops.secrets."k3s.token".path;
     serverAddr = "https://100.64.88.62:6443";
   };
   services.k3s-no-ctstate-invalid.enable = true;
