1376cbc4221b89caa8bed68d74badfb45f2a5ad2
imlonghao <git@imlonghao.com>
2021-12-28 11:55:26 +0800

    pkg(etherguard): bump

--- pkgs/etherguard/default.nix
+++ pkgs/etherguard/default.nix
@@ -5,6 +5,8 @@ buildGoModule rec {
 
   vendorSha256 = "sha256-f3F2xxMvNJ37lxM4VaQwZIhU5g8b3ukCwdpgePhZHPM=";
 
+  ldflags = [ "-s" "-w" "-X=main.Version=${version}" ];
+
   meta = with lib; {
     description = "Layer2 version of wireguard with Floyd Warshall implement in go";
     homepage = "https://github.com/KusakabeSi/EtherGuard-VPN";
