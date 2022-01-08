56668f0508745c12a723ad474718d95fb10678ee
imlonghao <git@imlonghao.com>
2022-01-08 12:00:11 +0800

    user(root): add hetzner2 ssh

--- users/root/default.nix
+++ users/root/default.nix
@@ -13,6 +13,7 @@ bWWiZuNxq92HpR9ddIJ62w47ZdtCrikX6GQW9xJOdEtNFEKz+Y9HYOL8uXOv8w1tnM3RDgc5a145ENpm
 lnUteXuaejULw== openpgp:0x1E1A60D7"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKv5fgCyrSdHw1z4Yvdi28fLs413vLFYk5sYyfC1YHJz imlonghao@imlonghao"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJdjuyWpE3wwjLVXpF/Rs0JjSk9Q6QHhQlrjspFzFA7M root@hetznerdefalkenstein1"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL9dyYPiJZc/TwNLcQBDDAVHGkpofq1rfgpd18VjrBwp root@hetznerdefalkenstein2"
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIPssSSxz4ZxCS/Tp4TSC4/6P0FQ+bvXWsrl3FTB5denvAAAABHNzaDo= CanoKey"
     ];
     shell = pkgs.fish;
