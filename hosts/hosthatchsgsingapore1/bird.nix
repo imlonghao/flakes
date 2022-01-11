ef9be866fa1dc089f42c05e5a5192a44428c19fd
imlonghao <git@imlonghao.com>
2022-01-11 20:38:04 +0800

    host(sg): peer 4242420253

--- hosts/hosthatchsgsingapore1/bird.nix
+++ hosts/hosthatchsgsingapore1/bird.nix
@@ -132,6 +132,9 @@
       protocol bgp AS4201271111 from dnpeers {
         neighbor fe80::aa:1111:11 % 'wg31111' as 4201271111;
       }
+      protocol bgp AS4242420253 from dnpeers {
+        neighbor fe80::0253 % 'wg0253' as 4242420253;
+      }
       protocol bgp AS4242420604 from dnpeers {
         neighbor fe80::0604 % 'wg0604' as 4242420604;
       }
--- hosts/hosthatchsgsingapore1/wireguard.nix
+++ hosts/hosthatchsgsingapore1/wireguard.nix
@@ -15,6 +15,19 @@
         }
       ];
     };
+    wg0253 = {
+      ips = [ "fe80::1888/64" ];
+      privateKeyFile = config.sops.secrets.wireguard.path;
+      listenPort = 20253;
+      allowedIPsAsRoutes = false;
+      peers = [
+        {
+          endpoint = "sg1.dn42.moe233.net:21888";
+          publicKey = "beaS4l4XBT4eSZBLJcu/u6fbm0mRk0TfuSLkMp8jOkY=";
+          allowedIPs = [ "10.0.0.0/8" "172.20.0.0/14" "172.31.0.0/16" "fe80::/64" "fd00::/8" ];
+        }
+      ];
+    };
     wg0604 = {
       ips = [ "fe80::1888/64" ];
       postSetup = "${pkgs.iproute2}/bin/ip addr add 172.22.68.0/32 peer 172.23.89.1/32 dev wg0604";
