55889ab40d3b28060007582c0b17e14177bda548
imlonghao <git@imlonghao.com>
2022-01-14 01:29:12 +0800

    host(se): add group and type

--- hosts/hosthatchsestockholm1/borg.nix
+++ hosts/hosthatchsestockholm1/borg.nix
@@ -3,9 +3,10 @@
 {
   # test user
   users.users.gikzoncf = {
-    uid = 5001;
+    group = "gikzoncf";
     home = "/persist/borg/gikzoncf";
     createHome = true;
+    isNormalUser = true;
     openssh.authorizedKeys.keys = [
       ''command="${pkgs.borgbackup}/bin/borg serve --restrict-to-repository /persist/borg/gikzoncf/repo --append-only",restrict ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKv5fgCyrSdHw1z4Yvdi28fLs413vLFYk5sYyfC1YHJz''
     ];
