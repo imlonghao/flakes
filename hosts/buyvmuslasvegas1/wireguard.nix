ade558c5f6974cce3334614e5c34b9ba141809f2
imlonghao <git@imlonghao.com>
2022-02-20 10:51:07 +0800

    host(us2): peer 4242421586

--- hosts/buyvmuslasvegas1/wireguard.nix
+++ hosts/buyvmuslasvegas1/wireguard.nix
@@ -58,6 +58,20 @@
         }
       ];
     };
+    wg1586 = {
+      ips = [ "fe80::1888/64" ];
+      postSetup = "${pkgs.iproute2}/bin/ip addr add 172.22.68.0/32 peer 172.21.75.32/32 dev wg1586";
+      privateKeyFile = config.sops.secrets.wireguard.path;
+      listenPort = 21586;
+      allowedIPsAsRoutes = false;
+      peers = [
+        {
+          endpoint = "lv.dn42.serversc.com:21888";
+          publicKey = "9CO2NAA7vDqnkHP1ro4c0+zm6wEHmdaAF3JMShfTlkA=";
+          allowedIPs = [ "10.0.0.0/8" "172.20.0.0/14" "172.31.0.0/16" "fe80::/64" "fd00::/8" ];
+        }
+      ];
+    };
     wg1817 = {
       ips = [ "fe80::1888/64" ];
       privateKeyFile = config.sops.secrets.wireguard.path;
