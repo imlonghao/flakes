eeae30deb27e8108375e53f6bca4300b520786a2
imlonghao <git@imlonghao.com>
2022-07-15 21:51:03 +0800

    host(us2): peering 4242420864

--- hosts/buyvmuslasvegas1/wireguard.nix
+++ hosts/buyvmuslasvegas1/wireguard.nix
@@ -58,6 +58,19 @@
         }
       ];
     };
+    wg0864 = {
+      ips = [ "fe80::1888/64" ];
+      privateKeyFile = config.sops.secrets.wireguard.path;
+      listenPort = 20864;
+      allowedIPsAsRoutes = false;
+      peers = [
+        {
+          endpoint = "lax.dn42.christine.pp.ua:21888";
+          publicKey = "mOQs7kIucUmSDXqRHvwfUxLAFkUDg9ssH5Gqn+6oj0s=";
+          allowedIPs = [ "10.0.0.0/8" "172.20.0.0/14" "172.31.0.0/16" "fe80::/64" "fd00::/8" ];
+        }
+      ];
+    };
     wg1123 = {
       ips = [ "fe80::1888/64" ];
       privateKeyFile = config.sops.secrets.wireguard.path;
