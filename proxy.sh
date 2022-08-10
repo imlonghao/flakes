493f772a40c94794f614ce82c4a66f166e62cb3c
imlonghao <git@imlonghao.com>
2022-08-11 00:34:05 +0800

    proxy: update

--- proxy.sh
+++ proxy.sh
@@ -1,5 +1,5 @@
-export CURL_NIX_FLAGS="-x http://127.0.0.1:8118/"
-export HTTP_PROXY=http://127.0.0.1:8118/
-export HTTPS_PROXY=http://127.0.0.1:8118/
-export http_proxy=http://127.0.0.1:8118/
-export https_proxy=http://127.0.0.1:8118/
+export CURL_NIX_FLAGS="-x http://192.168.123.1:7890/"
+export HTTP_PROXY=http://192.168.123.1:7890/
+export HTTPS_PROXY=http://192.168.123.1:7890/
+export http_proxy=http://192.168.123.1:7890/
+export https_proxy=http://192.168.123.1:7890/
