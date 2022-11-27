a9de683adb06d751c150e26cf577c41dc34550fb
imlonghao <git@imlonghao.com>
2022-11-27 16:06:40 +0800

    host(se): add bord user

--- hosts/hosthatchsestockholm1/borg.nix
+++ hosts/hosthatchsestockholm1/borg.nix
@@ -1,13 +1,24 @@
 { pkgs, ... }:
 
 {
-  users.users.vpsjptokyo1 = {
-    group = "nogroup";
-    home = "/persist/borg/vpsjptokyo1";
-    createHome = true;
-    isNormalUser = true;
-    openssh.authorizedKeys.keys = [
-      ''command="${pkgs.borgbackup}/bin/borg serve --restrict-to-repository /persist/borg/vpsjptokyo1/repo --append-only",restrict ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMCayvEz2OVsmPx3UUHOTO4OvOJ9oqr19mUtaWSsQ7EH root@vpsjptokyo1''
-    ];
+  users.users = {
+    vpsjptokyo1 = {
+      group = "nogroup";
+      home = "/persist/borg/vpsjptokyo1";
+      createHome = true;
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+        ''command="${pkgs.borgbackup}/bin/borg serve --restrict-to-repository /persist/borg/vpsjptokyo1/repo --append-only",restrict ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMCayvEz2OVsmPx3UUHOTO4OvOJ9oqr19mUtaWSsQ7EH root@vpsjptokyo1''
+      ];
+    };
+    cloudiplcuslosangeles1 = {
+      group = "nogroup";
+      home = "/persist/borg/cloudiplcuslosangeles1";
+      createHome = true;
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+        ''command="${pkgs.borgbackup}/bin/borg serve --restrict-to-repository /persist/borg/vpsjptokyo1/repo --append-only",restrict ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOd9q7yYhjYQcxiSmybWtbHYa/cnwK/uz/fYP4mVV0AH root@cloudiplcuslosangeles1''
+      ];
+    };
   };
 }
