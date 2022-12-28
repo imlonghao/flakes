7d6c2fb86d410bd1a2cd381d6e00ca6904fd50eb
imlonghao <git@imlonghao.com>
2022-12-28 20:55:54 +0800

    host(au): move to new config

--- hosts/vpsausydney1/bird.nix
+++ hosts/vpsausydney1/bird.nix
@@ -10,21 +10,11 @@ let
       route fd21:5c0c:9b7e:9::/64 blackhole;
     '';
   };
-  dn42Conf = import profiles.bird.dn42 { region = 53; country = 1036; ip = 9; };
+  dn42Conf = import profiles.bird.dn42 { region = 53; country = 1036; ip = 9; config = config; lib = lib; };
 in
 {
   services.bird2 = {
     enable = true;
-    config = generalConf + dn42Conf + ''
-      protocol bgp AS4242420458 from dnpeers {
-        neighbor fe80::458 % 'wg0458' as 4242420458;
-      }
-      protocol bgp AS4242421080 from dnpeers {
-        neighbor fe80::1080:125 % 'wg1080' as 4242421080;
-      }
-      protocol bgp AS4242422633 from dnpeers {
-        neighbor fe80::2633 % 'wg2633' as 4242422633;
-      }
-    '';
+    config = generalConf + dn42Conf;
   };
 }
