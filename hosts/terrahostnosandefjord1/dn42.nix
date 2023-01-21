4ff7c2b081cf0ca5b631af2969043684bb52bf85
imlonghao <git@imlonghao.com>
2023-01-21 22:36:10 +0800

    host(no): peer 4242421816

--- hosts/terrahostnosandefjord1/dn42.nix
+++ hosts/terrahostnosandefjord1/dn42.nix
@@ -3,5 +3,6 @@
 {
   sops.secrets.wireguard.sopsFile = ./secrets.yml;
   dn42 = [
+    { name = "wg1816"; listen = 21816; endpoint = "no1.dn42.potat0.cc:21888"; publickey = "H6HdsuQsav9puKyo8SJaML0vPU/a2lLQjTRc7dmiqjs="; asn = 4242421816; e6 = "fe80::1816"; }
   ];
 }
