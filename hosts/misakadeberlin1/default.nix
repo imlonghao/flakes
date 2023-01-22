1a839f5df8da0398233a926f11add0e75349c9de
imlonghao <git@imlonghao.com>
2023-01-22 20:26:14 +0800

    host(berlin): add goedge-admin

--- hosts/misakadeberlin1/default.nix
+++ hosts/misakadeberlin1/default.nix
@@ -1,6 +1,10 @@
 { pkgs, profiles, ... }:
 let
   hostCertificate = pkgs.writeText "ssh_host_ed25519_key-cert.pub" "ssh-ed25519-cert-v01@openssh.com AAAAIHNzaC1lZDI1NTE5LWNlcnQtdjAxQG9wZW5zc2guY29tAAAAIOXt5Ky4HgllBx6lN2qr+0zYXeMYt5+e8SnwUlSYUYQfAAAAIF3/kKhE8t/be9VMk7RJgd9/anolFA/bksy+Hs8Kc6/rAAAAAAAAAAAAAAACAAAAD21pc2FrYWRlYmVybGluMQAAAAAAAAAAAAAAAP//////////AAAAAAAAAAAAAAAAAAAAaAAAABNlY2RzYS1zaGEyLW5pc3RwMjU2AAAACG5pc3RwMjU2AAAAQQTuRtglhDg1ZegySmMt+nKOieitdmPjn7Ql1IoYRqbymyjTOf7yJjU8A8wMgiqynDPA2vtVkyCZyGTPapSxvGXWAAAAYwAAABNlY2RzYS1zaGEyLW5pc3RwMjU2AAAASAAAACADA7ypUvCpnibnSRdLL9H/fYcxMqQMz3alEVyoamOqbwAAACAPu2XuPY7bMhWmlr1PhQ6DgS7wF1XDxFlJRTIBE4iq8w==";
+  cronJob = pkgs.writeShellScript "cron.sh" ''
+    # GoEdge
+    /persist/edge-admin/bin/edge-admin start
+  '';
 in
 {
   imports = [
@@ -42,4 +46,18 @@ in
   services.openssh.extraConfig = ''
     HostCertificate = ${hostCertificate}
   '';
+
+  # Crontab
+  services.cron = {
+    enable = true;
+    systemCronJobs = [
+      "* * * * * root ${cronJob} > /dev/null 2>&1"
+    ];
+  };
+
+  # Mariadb
+  services.mysql = {
+    enable = true;
+    package = pkgs.mariadb;
+  };
 }
