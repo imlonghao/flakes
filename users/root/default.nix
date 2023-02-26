6463fe02f7f9672840a618cb0a3ad3ab1316f72b
imlonghao <git@imlonghao.com>
2023-02-26 16:31:47 +0800

    user(root): add ovh ssh pkey

--- users/root/default.nix
+++ users/root/default.nix
@@ -8,7 +8,7 @@
     passwordFile = config.sops.secrets.root.path;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKv5fgCyrSdHw1z4Yvdi28fLs413vLFYk5sYyfC1YHJz imlonghao@imlonghao"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJdjuyWpE3wwjLVXpF/Rs0JjSk9Q6QHhQlrjspFzFA7M root@hetznerdefalkenstein1"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOeDCr6dY6ScASSNauNDZx1hOjTg80Ih5LWW4z0HDDS2 root@ovhfrgravelines1"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIL9dyYPiJZc/TwNLcQBDDAVHGkpofq1rfgpd18VjrBwp root@hetznerdefalkenstein2"
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIPssSSxz4ZxCS/Tp4TSC4/6P0FQ+bvXWsrl3FTB5denvAAAABHNzaDo= CanoKey"
     ];
