d6ce76c1299d46cfcfafd3bace5e71a4d04fe411
imlonghao <git@imlonghao.com>
2023-03-04 23:24:14 +0800

    profile(bird): add krt_prefsrc

--- profiles/bird/kernel.nix
+++ profiles/bird/kernel.nix
@@ -1,3 +1,8 @@
+{ src4 ? "", src6 ? "" }:
+let
+  ssrc4 = if src4 == "" then "" else "krt_prefsrc = ${src4};";
+  ssrc6 = if src6 == "" then "" else "krt_prefsrc = ${src6};";
+in
 ''
   protocol kernel {
     scan time 10;
@@ -7,6 +12,7 @@
       export filter {
         if net = 0.0.0.0/0 then reject;
         if source = RTS_DEVICE then reject;
+        ${ssrc4}
         accept;
       };
     };
@@ -19,6 +25,7 @@
       export filter {
         if net = ::/0 then reject;
         if source = RTS_DEVICE then reject;
+        ${ssrc6}
         accept;
       };
     };
