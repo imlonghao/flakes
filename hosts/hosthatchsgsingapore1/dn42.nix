a3da496003d9545d26079c90084747451f931862
imlonghao <git@imlonghao.com>
2023-03-05 21:40:44 +0800

    host(sg): peer 4242422271

--- hosts/hosthatchsgsingapore1/dn42.nix
+++ hosts/hosthatchsgsingapore1/dn42.nix
@@ -11,6 +11,7 @@
     { name = "wg1255"; listen = 21255; endpoint = "dn42-sg-sin4.acgcl.net:31888"; publickey = "dJ7poThkXSLU1BTG9ghTVHlDaKT0Snz3YmzfRdPLKQU="; asn = 4242421255; e6 = "fe80::1020"; }
     { name = "wg1588"; listen = 21588; endpoint = "sg-sin01.dn42.tech9.io:59771"; publickey = "4qLIJ9zpc/Xgvy+uo90rGso75cSrT2F5tBEv+6aqDkY="; asn = 4242421588; ipv6 = "fe80::100/64"; e4 = "172.20.16.142"; e6 = "fe80::1588"; l4 = "172.22.68.2"; }
     { name = "wg2237"; listen = 22237; endpoint = "sg-sin01.dn42.munsternet.eu:21888"; publickey = "09m8ilgZ/9jQvVgsGwu2ceR8u6gKAsd+VxH8AzduOHk="; asn = 4242422237; e6 = "fe80::42:2237"; }
+    { name = "wg2271"; listen = 22271; endpoint = "185.212.61.190:21888"; publickey = "9RBImDH9X0NghJsVW6hAVQS1qaR+RXrsbVXisBAViHE="; asn = 4242422271; e6 = "fe80::2271"; }
     { name = "wg2331"; listen = 22331; endpoint = "103.83.156.22:21888"; publickey = "I5yRgHFY+qfkRwT6UpVBsUIiA5hmEOv1cU2licfrokw="; asn = 4242422331; e6 = "fe80::2331"; }
     { name = "wg2633"; listen = 22633; endpoint = "sin.eastbnd.com:21888"; publickey = "m5IfciUmvMEfDkfFQf0jD3GH0F0ChMktOSiLMlJ29wc="; asn = 4242422633; e4 = "172.23.250.34"; e6 = "fe80::2633"; l4 = "172.22.68.2"; }
     { name = "wg2717"; listen = 22717; endpoint = "sg.vm.whojk.com:24103"; publickey = "vCtn1DbfIiTgcMapuEGB/+/HnLeEPKPjxJbt/sjviTs="; asn = 4242422717; e6 = "fe80::2717"; }
