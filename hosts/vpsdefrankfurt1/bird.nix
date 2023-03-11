365ba785a6ccc418d67b703db526a087fbd603c1
imlonghao <git@imlonghao.com>
2023-03-11 20:57:12 +0800

    host(fra): use bgp_large_community

--- hosts/vpsdefrankfurt1/bird.nix
+++ hosts/vpsdefrankfurt1/bird.nix
@@ -2,10 +2,6 @@
 let
   generalConf = import profiles.bird.general {
     config = config;
-    route6 = ''
-      route 2602:fab0:20::/48 blackhole;
-      route 2602:fab0:22::/48 blackhole;
-    '';
   };
   kernelConf = import profiles.bird.kernel {
     src6 = "2602:fab0:22::";
@@ -15,6 +11,20 @@ in
   services.bird2 = {
     enable = true;
     config = generalConf + kernelConf + ''
+      protocol static {
+        route 2602:fab0:20::/48 blackhole;
+        route 2602:fab0:22::/48 blackhole;
+        ipv6 {
+          import filter {
+            bgp_large_community.add((199632, 1, 1));
+            bgp_large_community.add((199632, 2, 1));
+            bgp_large_community.add((199632, 3, 276));
+            accept;
+          };
+          export all;
+        };
+      }
+
       # bgpq4 -S RPKI,AFRINIC,ARIN,APNIC,LACNIC,RIPE -6Ab -l AS134993 -R 48 APNIC::AS-ILEMONRAIN
       define 'APNIC::AS-ILEMONRAIN' = [
         2406:840:fd00::/43{43,48},
@@ -25,51 +35,72 @@ in
         local as 199632;
         graceful restart on;
         ipv6 {
-          import all;
-          export where net ~ [2602:fab0:20::/48, 2602:fab0:22::/48];
+          import filter {
+            bgp_large_community.add((199632, 1, 2));
+            bgp_large_community.add((199632, 2, 1));
+            bgp_large_community.add((199632, 3, 276));
+            accept;
+          };
+          export where bgp_large_community ~ [(199632, 1, 1), (199632, 1, 5)];
         };
       }
-      protocol bgp AS112 from tmpl_upstream {
-        neighbor 2001:7f8:f2:e1::112 as 112;
-      };
+      template bgp tmpl_rs {
+        local as 199632;
+        graceful restart on;
+        ipv6 {
+          import filter {
+            bgp_large_community.add((199632, 1, 4));
+            bgp_large_community.add((199632, 2, 1));
+            bgp_large_community.add((199632, 3, 276));
+            accept;
+          };
+          export where bgp_large_community ~ [(199632, 1, 1), (199632, 1, 5)];
+        };
+      }
+      template bgp tmpl_peer {
+        local as 199632;
+        graceful restart on;
+        ipv6 {
+          export where bgp_large_community ~ [(199632, 1, 1), (199632, 1, 5)];
+        };
+      }
+
       protocol bgp AS3204 from tmpl_upstream {
         neighbor 2a09:0:9::9 as 3204;
         multihop 2;
         password "aku236991uha";
       };
-      protocol bgp AS6939 {
+      protocol bgp AS6939 from tmpl_upstream {
         neighbor 2001:7f8:f2:e1::6939:1 as 6939;
-        local as 199632;
-        graceful restart on;
         ipv6 {
-          import all;
           export filter {
             if net = 2602:fab0:20::/48 then {
               bgp_path.prepend(199632);
               accept;
             }
-            if net = 2602:fab0:22::/48 then accept;
-            reject;
+            if bgp_large_community ~ [(199632, 1, 1), (199632, 1, 5)] then accept;
           };
         };
       };
-      protocol bgp AS134993 {
+      protocol bgp AS134993 from tmpl_peer {
         neighbor 2001:7f8:f2:e1:0:1349:93:1 as 134993;
         description "iLemonrain Network";
-        local as 199632;
-        graceful restart on;
         ipv6 {
-          import where net ~ 'APNIC::AS-ILEMONRAIN';
-          export where net ~ [2602:fab0:20::/48, 2602:fab0:22::/48];
+          import filter {
+            bgp_large_community.add((199632, 1, 3));
+            bgp_large_community.add((199632, 2, 1));
+            bgp_large_community.add((199632, 3, 276));
+            if net ~ 'APNIC::AS-ILEMONRAIN' then accept;
+          };
         };
       };
-      protocol bgp AS202409rs01 from tmpl_upstream {
+      protocol bgp AS202409rs01 from tmpl_rs {
         neighbor 2001:7f8:f2:e1::babe:1 as 202409;
       };
-      protocol bgp AS202409rs02 from tmpl_upstream {
+      protocol bgp AS202409rs02 from tmpl_rs {
         neighbor 2001:7f8:f2:e1::dead:1 as 202409;
       };
-      protocol bgp AS202409rs03 from tmpl_upstream {
+      protocol bgp AS202409rs03 from tmpl_rs {
         neighbor 2001:7f8:f2:e1::be5a as 202409;
       };
     '';
