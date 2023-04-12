2c39a8c8ed1a81a550ff7fa3b163de19f16fb906
imlonghao <git@imlonghao.com>
2023-04-12 22:26:52 +0800

    host(sg): auto update certs

--- hosts/hosthatchsgsingapore1/default.nix
+++ hosts/hosthatchsgsingapore1/default.nix
@@ -201,6 +201,7 @@ in
     enable = true;
     systemCronJobs = [
       "* * * * * root ${cronJob} > /dev/null 2>&1"
+      "0 1 * * * root ${pkgs.git}/bin/git -C /persist/pki pull"
     ];
   };
 }
