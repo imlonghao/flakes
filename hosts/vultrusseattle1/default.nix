207e86bfb7d01f0116c73a1fe0c13fe38ad8eb59
imlonghao <git@imlonghao.com>
2023-04-15 14:50:05 +0800

    host(sea): init

---
+++ hosts/vultrusseattle1/default.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, profiles, self, sops, ... }:
+let
+  hostCertificate = pkgs.writeText "ssh_host_ed25519_key-cert.pub" "ssh-ed25519-cert-v01@openssh.com AAAAIHNzaC1lZDI1NTE5LWNlcnQtdjAxQG9wZW5zc2guY29tAAAAIINVs+LD+1BP0deuPA07tuoYVMlQftbmiZIbdv4K5sfzAAAAICoi3lWKYf9PdO/c9uSf5rty9CdjU9Sr14UuF9HPRdCXAAAAAAAAAAAAAAACAAAAD3Z1bHRydXNzZWF0dGxlMQAAAAAAAAAAAAAAAP//////////AAAAAAAAAAAAAAAAAAAAaAAAABNlY2RzYS1zaGEyLW5pc3RwMjU2AAAACG5pc3RwMjU2AAAAQQTuRtglhDg1ZegySmMt+nKOieitdmPjn7Ql1IoYRqbymyjTOf7yJjU8A8wMgiqynDPA2vtVkyCZyGTPapSxvGXWAAAAZQAAABNlY2RzYS1zaGEyLW5pc3RwMjU2AAAASgAAACEAuX2/V4UwPirHl7Ub7i7S4yVjDSkYgAP+z0BhTJjBWpoAAAAhAMzWxwAEkbFBuVa7dxFSuhCILeGqSjgfVbI1vSZkdUl0";
+in
+{
+  imports = [
+    ./hardware.nix
+    profiles.mycore
+    profiles.users.root
+  ];
+
+  networking = {
+    interfaces = {
+      lo = {
+        ipv6.addresses = [
+          { address = "2602:fab0:20::"; prefixLength = 128; }
+          { address = "2602:fab0:25::"; prefixLength = 128; }
+        ];
+      };
+      enp1s0.useDHCP = true;
+    };
+  };
+
+  # OpenSSH
+  services.openssh.extraConfig = ''
+    HostCertificate = ${hostCertificate}
+  '';
+
+}
