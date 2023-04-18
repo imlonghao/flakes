4a6b8029e5cb49808bc92fa8387c9a082bfadeaf
imlonghao <git@imlonghao.com>
2023-04-18 16:31:26 +0800

    profile(borgmatic): temp fix

--- profiles/borgmatic/default.nix
+++ profiles/borgmatic/default.nix
@@ -5,6 +5,7 @@
 
   sops.secrets.borgmatic.sopsFile = "${self}/hosts/${config.networking.hostName}/secrets.yml";
   systemd.services.borgmatic.serviceConfig.EnvironmentFile = "/run/secrets/borgmatic";
+  systemd.timers.borgmatic.wantedBy =  [ "timers.target" ];
 
   services.borgmatic = {
     enable = true;
