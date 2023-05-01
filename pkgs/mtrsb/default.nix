1264340c177ba529b9455989cd71598ded1a1e8b
imlonghao <git@imlonghao.com>
2023-05-01 16:19:59 +0800

    pkg(mtrsb): add dep iputils

--- pkgs/mtrsb/default.nix
+++ pkgs/mtrsb/default.nix
@@ -2,6 +2,9 @@
 
 buildGoModule rec {
   inherit (sources.mtrsb) pname version src vendorSha256;
+  propagatedBuildInputs = with pkgs; [
+    iputils
+  ];
   meta = with lib; {
     description = "MTR.SB Worker";
     license = licenses.mit;
