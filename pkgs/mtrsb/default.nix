a85c111e86b82f016c6116b1669f19fd035aab0f
imlonghao <git@imlonghao.com>
2023-05-01 21:08:33 +0800

    pkg(mtrsb): add version flag

--- pkgs/mtrsb/default.nix
+++ pkgs/mtrsb/default.nix
@@ -3,6 +3,7 @@
 buildGoModule rec {
   inherit (sources.mtrsb) pname version src vendorSha256;
   subPackages = [ "cmd/worker" ];
+  ldflags = [ "-s" "-w" "-X main.Version=${version}" ];
   nativeBuildInputs = [ makeWrapper ];
   postInstall = ''
     wrapProgram $out/bin/worker --suffix PATH : ${lib.makeBinPath [ pkgs.iputils ]}
