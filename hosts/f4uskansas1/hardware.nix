74b6f5e86c7b09cfd5756619363ffd19c3f2be89
imlonghao <git@imlonghao.com>
2023-07-08 13:25:59 +0800

    host(f4): fix boot.loader.grub.device

--- hosts/f4uskansas1/hardware.nix
+++ hosts/f4uskansas1/hardware.nix
@@ -7,7 +7,7 @@
 
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "sym53c8xx" "ahci" "sd_mod" "sr_mod" ];
   boot.kernelModules = [ "kvm-intel" ];
-  boot.loader.grub.devices = "/dev/sda";
+  boot.loader.grub.device = "/dev/sda";
 
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/eb2fc67c-5aed-4dcb-81d5-2c314692ce27";
