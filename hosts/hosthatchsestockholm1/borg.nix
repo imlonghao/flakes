1f8c5be063a62c28722676c771fe61efbdb0144c
imlonghao <git@imlonghao.com>
2023-09-02 11:33:29 +0800

    host(se): borg add nas

--- hosts/hosthatchsestockholm1/borg.nix
+++ hosts/hosthatchsestockholm1/borg.nix
@@ -20,5 +20,14 @@
         ''command="${pkgs.borgbackup}/bin/borg serve --restrict-to-repository /persist/borg/cloudiplcuslosangeles1/repo --append-only",restrict ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOd9q7yYhjYQcxiSmybWtbHYa/cnwK/uz/fYP4mVV0AH root@cloudiplcuslosangeles1''
       ];
     };
+    nas = {
+      group = "nogroup";
+      home = "/persist/borg/nas";
+      createHome = true;
+      isNormalUser = true;
+      openssh.authorizedKeys.keys = [
+        ''command="${pkgs.borgbackup}/bin/borg serve --restrict-to-repository /persist/borg/nas/repo --append-only",restrict ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA+HDK17YPUx571+Q3qzxC81SJv8Taj+HeCHLhsEUlTG root@nas''
+      ];
+    };
   };
 }
