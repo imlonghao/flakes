5df89f94456f8bee219071d1b29946e0c0b56839
imlonghao <git@imlonghao.com>
2023-11-24 23:27:00 +0800

    host(lu): umount /rtorrent

--- hosts/buyvmluroost1/hardware.nix
+++ hosts/buyvmluroost1/hardware.nix
@@ -5,5 +5,4 @@
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" "vmw_pvscsi" ];
   boot.initrd.kernelModules = [ "nvme" ];
   fileSystems."/" = { device = "/dev/vda1"; fsType = "ext4"; };
-  fileSystems."/rtorrent" = { device = "/dev/sda1"; fsType = "ext4"; };
 }
