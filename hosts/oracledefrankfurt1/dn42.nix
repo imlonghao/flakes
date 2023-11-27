241f4626bc026660daaabbaf5a227ecaf97c17f6
imlonghao <git@imlonghao.com>
2023-11-27 21:04:01 +0800

    host(de): peer 4242422596

--- hosts/oracledefrankfurt1/dn42.nix
+++ hosts/oracledefrankfurt1/dn42.nix
@@ -19,6 +19,7 @@
     { name = "wg2189"; listen = 22189; endpoint = "de-fra.dn42.kuu.moe:57353"; publickey = "FHp0OR4UpAS8/Ra0FUNffTk18soUYCa6NcvZdOgxY0k="; asn = 4242422189; e6 = "fe80::2189:e9"; }
     { name = "wg2331"; listen = 22331; endpoint = "lu208.dn42.williamgates.info:21888"; publickey = "c4AZZVNUzXCASWG96CKUpY+gQLdGwA1rbqkYCHXnW10="; asn = 4242422331; e6 = "fe80::2331"; }
     { name = "wg2458"; listen = 22458; endpoint = "nl-ams-a.nodes.pigeonhole.eu.org:51888"; publickey = "QJnmWUnPS9wKKkLHHuWBYMGAI20MH9OEo28O4qr5DV8="; asn = 4242422458; e6 = "fe80::2458"; }
+    { name = "wg2596"; listen = 22596; endpoint = "redxen.chaox.ro:21888"; publickey = "N6qNsK3fAqj9hENxZqUJePnyQMgyrzxoCdKb2xEvzjM="; asn = 4242422596; e6 = "fe80::2596:3"; }
     { name = "wg2717"; listen = 22717; endpoint = "nl.vm.whojk.com:23024"; publickey = "cokP4jFBH0TlBD/m3sWCpc9nADLOhzM2+lcjAb3ynFc="; asn = 4242422717; e6 = "fe80::2717"; }
     { name = "wg2923"; listen = 22923; endpoint = "p2p-node.de:51888"; publickey = "GD554w8JM8R2s0c/mR7sBbYy/zTP5GWWB+Zl1gx5GUk="; asn = 4242422923; e6 = "fe80::2923"; }
     { name = "wg2980"; listen = 22980; endpoint = "fra1.de.dn42.yuuta.moe:21888"; publickey = "GYEhSHmPD0pVX3xBKa7SAwnuCyMA2oOsaHBgFpPO4X4="; presharedkey = "iHxtuu7sFtvR/nsOA2m3T4Lt3w8P4VzvLKHWAm23a1w="; asn = 4242422980; ipv6 = "fe80::1888:4/64"; e6 = "fe80::2980"; }
