eade8c92eca0bbbb6ae30d11ab7ea979db840be6
imlonghao <git@imlonghao.com>
2023-12-10 10:17:15 +0800

    host(de): update docker config

--- hosts/misakadeberlin1/default.nix
+++ hosts/misakadeberlin1/default.nix
@@ -90,8 +90,18 @@ in
   };
 
   # Docker
-  virtualisation.docker.enable = true;
-  virtualisation.docker.daemon.settings = {
-    userland-proxy = false;
+  virtualisation.docker = {
+    enable = true;
+    daemon.settings = {
+      default-address-pools = [
+        {
+          "base" = "100.65.0.0/16";
+          "size" = 24;
+        }
+      ];
+#      userland-proxy = false;
+      experimental = true;
+      ip6tables = true;
+    };
   };
 }
