8a8944cff3a3f1cf60acadd6d18f4054eb34ff07
imlonghao <git@imlonghao.com>
2023-12-10 10:16:49 +0800

    hosts: fix 4242420585 endpoint

--- hosts/hosthatchsgsingapore1/dn42.nix
+++ hosts/hosthatchsgsingapore1/dn42.nix
@@ -6,7 +6,7 @@
     { name = "wg31111"; listen = 31111; publickey = "7TIbiifNzh8HxLUM8cBvwmBo/kuaCAUCRahbBMoVA1Q="; asn = 4201271111; e6 = "fe80::aa:1111:11"; }
     { name = "wg0207"; listen = 20207; endpoint = "router.sin1.routedbits.com:51888"; publickey = "8sTnWN7ykPaDe1WIZOlnqECiGMJzwXB8TXWHcT7qnEw="; asn = 4242420207; e6 = "fe80::0207"; }
     { name = "wg0458"; listen = 20458; endpoint = "sg1.nodes.huajinet.org:21888"; publickey = "uVQSqDCJZzbMND7rRmoOR3QpANgl2SbTlv28aw0kh3w="; asn = 4242420458; e6 = "fe80::0458"; }
-    { name = "wg0585"; listen = 20585; endpoint = "de1.dn42.atolm.net:21888"; publickey = "r5u1s5OY8ISJqv7oc1ZCA7RXMrJ/UOMgFdFVZ7qaPi4="; asn = 4242420585; e6 = "fe80::585"; }
+    { name = "wg0585"; listen = 20585; endpoint = "sg1.dn42.atolm.net:21888"; publickey = "nI/itxRxKHntsWiBpawMZe2vOFho1rrOJdK7th0ZxFg="; asn = 4242420585; e6 = "fe80::585"; }
     { name = "wg0604"; listen = 20604; endpoint = "sgp1.dn42.cas7.moe:21888"; publickey = "R8iyaSzF6xx/t4+1wKlYWZWyZOxJDCXlA2BE3OZnsAY="; asn = 4242420604; e6 = "fe80::0604"; }
     { name = "wg0831"; listen = 20831; endpoint = "sg.dn42.tms.im:21888"; publickey = "KlZg3oOjQsaQ0dNkUgHCKyOqULw8+u+llo97X1w5mV4="; asn = 4242420831; e6 = "fe80::0831"; }
     { name = "wg1080"; listen = 21080; endpoint = "sgp.peer.highdef.network:21888"; publickey = "X3m9VMzZYN4Oe2QUb7DcnmVymwKSLbPUCB5ElD8igjo="; asn = 4242421080; e6 = "fe80::1080:39"; }
