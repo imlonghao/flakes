e94de029d47173368b86bd4de852b5254ec109f7
imlonghao <git@imlonghao.com>
2023-12-11 22:24:16 +0800

    host(de): enable rsshc

--- hosts/oracledefrankfurt1/default.nix
+++ hosts/oracledefrankfurt1/default.nix
@@ -1,6 +1,5 @@
 { config, modulesPath, pkgs, profiles, self, ... }:
 let
-  hostCertificate = pkgs.writeText "ssh_host_ed25519_key-cert.pub" "ssh-ed25519-cert-v01@openssh.com AAAAIHNzaC1lZDI1NTE5LWNlcnQtdjAxQG9wZW5zc2guY29tAAAAIPxV2X/3y1IqlODxz8OMJxgv14yL/RDF/ezD4CHH4MoFAAAAIGPrYUbX2Zv3h9OfLjVu0H45ggfxX5SNLjZX+aEkUweKAAAAAAAAAAAAAAACAAAAEm9yYWNsZWRlZnJhbmtmdXJ0MQAAAAAAAAAAAAAAAP//////////AAAAAAAAAAAAAAAAAAAAaAAAABNlY2RzYS1zaGEyLW5pc3RwMjU2AAAACG5pc3RwMjU2AAAAQQTuRtglhDg1ZegySmMt+nKOieitdmPjn7Ql1IoYRqbymyjTOf7yJjU8A8wMgiqynDPA2vtVkyCZyGTPapSxvGXWAAAAZQAAABNlY2RzYS1zaGEyLW5pc3RwMjU2AAAASgAAACEA4lDqutOQ4OLZYthTZmia+36Z7sfveCEmiLaQxgZyG4oAAAAhAOSrLyY0Rt/jMHJzhyGf5XMSpnuSjTZU2wXOL2WGoPue";
   cronJob = pkgs.writeShellScript "cron.sh" ''
     # GoEdge
     /persist/edge-node/bin/edge-node start
@@ -19,6 +18,7 @@ in
     profiles.pingfinder
     profiles.bird-lg-go
     profiles.mtrsb
+    profiles.rsshc
   ];
 
   # Config
@@ -82,9 +82,6 @@ in
   };
 
   # OpenSSH
-  services.openssh.extraConfig = ''
-    HostCertificate = ${hostCertificate}
-  '';
   users.users.root = {
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINwH+SQ2Zn0yAjNrsXSIZL7ViulHom4LixUAZQ5e+DoW root@nixos"
