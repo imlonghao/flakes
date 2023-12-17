8f73191accf4b87befe59ab7a4e4ca4d10b0a306
imlonghao <git@imlonghao.com>
2023-12-17 11:53:14 +0800

    host(breeze): add grub2

--- hosts/breezehostusdallas1/hardware.nix
+++ hosts/breezehostusdallas1/hardware.nix
@@ -1,6 +1,8 @@
 {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
 
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
   boot.initrd.availableKernelModules =
     [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
