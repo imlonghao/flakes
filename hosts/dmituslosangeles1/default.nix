de6dfa7f6fa57eef2fc183afdac63358c40c4b16
imlonghao <git@imlonghao.com>
2024-01-29 22:23:32 +0800

    host(dmit): update IPv6

--- hosts/dmituslosangeles1/default.nix
+++ hosts/dmituslosangeles1/default.nix
@@ -14,14 +14,17 @@
     nameservers = [ "8.8.8.8" "1.1.1.1" ];
     dhcpcd.enable = false;
     defaultGateway = "154.17.16.1";
-    defaultGateway6 = "2605:52c0:2::1";
+    defaultGateway6 = {
+      address = "fe80::5c3c:9dff:feca:8922";
+      interface = "eth0";
+    };
     interfaces = {
       eth0 = {
         ipv4.addresses = [
           { address = "154.17.16.135"; prefixLength = 24; }
         ];
         ipv6.addresses = [
-          { address = "2605:52c0:2:4ad::"; prefixLength = 48; }
+          { address = "2605:52c0:2:4ad::1"; prefixLength = 64; }
         ];
       };
     };
