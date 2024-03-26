cd4e8913f5dc6f9c1020bf0763ed4e16a3ea6f93
imlonghao <git@imlonghao.com>
2024-03-26 22:26:29 +0800

    modules: update sleep time

--- modules/bird-lg-go.nix
+++ modules/bird-lg-go.nix
@@ -14,7 +14,10 @@ in
   config = mkIf cfg.enable {
     systemd.services.bird-lg-go = {
       serviceConfig = with pkgs;{
+        ExecStartPre = "${coreutils}/bin/sleep 10";
         ExecStart = "${bird-lg-go}/bin/proxy --listen ${cfg.listen} --bird /run/bird/bird.ctl --traceroute_bin ${traceroute}/bin/traceroute";
+        Restart = "on-failure";
+        RestartSec = 10;
       };
       wants = [ "network-online.target" ];
       after = [ "network-online.target" "etherguard-edge.service" ];
