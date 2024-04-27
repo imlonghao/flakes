66010b28efc24c627bdf187a7a08f9594179688a
imlonghao <git@imlonghao.com>
2024-04-27 12:59:00 +0800

    profile(blackbox): add config

--- profiles/exporter/blackbox.nix
+++ profiles/exporter/blackbox.nix
@@ -1,11 +1,21 @@
 { config, ... }:
 let
   ip = builtins.replaceStrings [ "/24" ] [ "" ] config.services.etherguard-edge.ipv4;
+  # https://github.com/prometheus/blackbox_exporter/blob/master/CONFIGURATION.md
+  blackboxConfig = {
+    modules = {
+      icmp = {
+        prober = "icmp";
+        timeout = "5s";
+      };
+    };
+  };
 in
 {
   services.prometheus.exporters.blackbox = {
     enable = true;
     listenAddress = ip;
+    configFile = pkgs.writeText "blackbox.yml" (builtins.toJSON blackboxConfig);
   };
   systemd.services."prometheus-blackbox-exporter".after = [ "etherguard-edge.service" ];
 }
