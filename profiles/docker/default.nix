9b5a72d7d1e927c04e285274f1ac281ac14b1299
imlonghao <git@imlonghao.com>
2024-07-11 21:48:51 +0800

    profile(docker): use docker 27 and enable IPv6

--- profiles/docker/default.nix
+++ profiles/docker/default.nix
@@ -1,14 +1,20 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = [ pkgs.netdata ];
   virtualisation.docker = {
     enable = true;
+    package = pkgs.docker_27;
     daemon.settings = {
-      "default-address-pools" = [{
-        "base" = "100.65.0.0/16";
-        "size" = 24;
-      }];
+      "default-address-pools" = [
+        {
+          "base" = "100.65.0.0/16";
+          "size" = 24;
+        }
+        {
+          "base" = "fc00:d0c1:e300::/48";
+          "size" = 64;
+        }
+      ];
     };
   };
 }
