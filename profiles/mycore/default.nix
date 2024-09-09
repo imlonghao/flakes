d1a2df296f5bfe9d3fc6cfdfb5c56af2bc3283ac
imlonghao <git@imlonghao.com>
2024-09-10 00:09:13 +0800

    profile(mycore): custom htop

--- profiles/mycore/default.nix
+++ profiles/mycore/default.nix
@@ -126,4 +126,16 @@ in {
     priority = 100;
   };
 
+  programs.htop = {
+    enable = true;
+    settings = {
+      column_meters_0 = "LeftCPUs Memory Swap CPU NetworkIO";
+      column_meter_modes_0 = "1 1 1 2 2";
+      column_meters_1 = "RightCPUs Tasks LoadAverage Uptime DiskIO";
+      column_meter_modes_1 = "1 2 2 2 2";
+      detailed_cpu_time = 1;
+      show_cpu_temperature = 1;
+    };
+  };
+
 }
