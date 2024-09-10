7d513ed560589198e78f189146c1666b3611301e
imlonghao <git@imlonghao.com>
2024-09-11 00:06:01 +0800

    module(mptcp): fix command

--- modules/mptcp.nix
+++ modules/mptcp.nix
@@ -22,12 +22,14 @@ in {
       serviceConfig = {
         Type = "oneshot";
         ExecStartPre =
-          "${pkgs.iproute2}/bin/ip mp l add_addr_accepted 8 subflows 8";
+          "${pkgs.iproute2}/bin/ip mptcp limits set add_addr_accepted 8 subflows 8";
         ExecStart = map (x:
-          "${pkgs.iproute2}/bin/ip mp e ${x.address} dev ${x.dev} id ${x.id}"
-          + (if x.port != null then " port ${x.port}" else "")) cfg.endpoint;
-        ExecStop =
-          map (x: "${pkgs.iproute2}/bin/ip mp e del id ${x.id}") cfg.endpoint;
+          "${pkgs.iproute2}/bin/ip mptcp endpoint add ${x.address} dev ${x.dev} id ${
+            toString x.id
+          }" + (if x.port != null then " port ${x.port}" else "")) cfg.endpoint;
+        ExecStop = map
+          (x: "${pkgs.iproute2}/bin/ip mptcp endpoint del id ${toString x.id}")
+          cfg.endpoint;
       };
       wants = [ "network-online.target" ];
       after = [ "network-online.target" ];
