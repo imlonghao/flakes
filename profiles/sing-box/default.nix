d420f1c61c55cea0cf5107533ca875f1fdbfd550
imlonghao <git@imlonghao.com>
2024-09-17 16:52:30 +0800

    module(sb): use ss

--- profiles/sing-box/default.nix
+++ profiles/sing-box/default.nix
@@ -2,32 +2,22 @@
   sops.secrets.sing-box-uuid.sopsFile = "${self}/secrets/sing-box.yml";
   sops.secrets.sing-box-password.sopsFile = "${self}/secrets/sing-box.yml";
   sops.secrets.sing-box-sni.sopsFile = "${self}/secrets/sing-box.yml";
+  sops.secrets.sing-box-encrypt.sopsFile = "${self}/secrets/sing-box.yml";
   services.sing-box = {
     enable = true;
     settings = {
       inbounds = [
         {
-          type = "shadowtls";
+          type = "shadowsocks";
           listen = "::";
           listen_port = 4443;
-          version = 3;
-          users = [{
-            name = "toor";
-            password = {
-              _secret = config.sops.secrets.sing-box-password.path;
-            };
-          }];
-          handshake = {
-            server = "i0.hdslb.com";
-            server_port = 443;
-          };
-          detour = "s5-in";
           tcp_multi_path = true;
-        }
-        {
-          type = "socks";
-          tag = "s5-in";
-          listen = "127.0.0.1";
+          method = "2022-blake3-aes-256-gcm";
+          password = { _secret = config.sops.secrets.sing-box-encrypt.path; };
+          multiplex = {
+            enabled = true;
+            padding = true;
+          };
         }
         {
           type = "tuic";
