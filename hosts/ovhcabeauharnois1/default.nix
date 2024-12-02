421065d8ecc4f439bb92e8c926ba0a0664d7b2fc
imlonghao <git@imlonghao.com>
2024-12-02 23:13:32 +0800

    host(bhs): persist files

--- hosts/ovhcabeauharnois1/default.nix
+++ hosts/ovhcabeauharnois1/default.nix
@@ -14,6 +14,15 @@
     dhcpcd.allowInterfaces = [ "eno1" ];
   };
 
+  environment.persistence."/persist" = {
+    directories = [ "/var/lib" "/root/.ssh" ];
+    files = [
+      "/etc/machine-id"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_ed25519_key"
+    ];
+  };
+
   # EtherGuard
   services.etherguard-edge = {
     ipv4 = "100.64.88.44/24";
