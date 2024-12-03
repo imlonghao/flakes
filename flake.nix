ee52cc5d6b361cfd22f12c219348d578eee2ac58
imlonghao <git@imlonghao.com>
2024-12-03 21:04:01 +0800

    nix: bump

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     "imlonghao.cachix.org-1:KGQ7+R1BXo2NsoeAxKLPfGAiHz5ofCmZO4hih7u/2Q8= nrdxp.cachix.org-1:Fc5PSqY2Jm1TrWfm88l6cvGWwz3s93c6IOifQWnhNW4= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
 
   inputs = {
-    nixos.url = "github:nixos/nixpkgs/release-24.05";
+    nixos.url = "github:nixos/nixpkgs/release-24.11";
     latest.url = "github:nixos/nixpkgs/nixos-unstable";
 
     digga.url = "github:divnix/digga";
@@ -18,7 +18,7 @@
     digga.inputs.deploy.follows = "deploy";
     digga.inputs.flake-utils-plus.follows = "flake-utils-plus";
 
-    home.url = "github:nix-community/home-manager/release-24.05";
+    home.url = "github:nix-community/home-manager/release-24.11";
     home.inputs.nixpkgs.follows = "nixos";
 
     deploy.url = "github:serokell/deploy-rs";
