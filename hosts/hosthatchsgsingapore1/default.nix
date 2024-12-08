5bf1c934ff895274f54d6f42cec2c8e22797e012
imlonghao <git@imlonghao.com>
2024-12-08 20:28:21 +0800

    host(sg): add wrap

--- hosts/hosthatchsgsingapore1/default.nix
+++ hosts/hosthatchsgsingapore1/default.nix
@@ -230,4 +230,27 @@
     DynamicUser = lib.mkForce false;
   };
 
+  # Wrap
+  sops.secrets.wrap.sopsFile = ./secrets.yml;
+  networking.wireguard.interfaces.wrap = {
+    table = "913335";
+    privateKeyFile = config.sops.secrets.wrap.path;
+    ips = [ "172.16.0.2/32" "2606:4700:110:8cd2:4dc2:3ed7:3305:10cd/128" ];
+    mtu = 1420;
+    postSetup = [
+      "${pkgs.iproute2}/bin/ip rule add from 10.133.35.0/24 table 913335"
+      "${pkgs.iproute2}/bin/ip -6 rule add from 133:35::/64 table 913335"
+    ];
+    postShutdown = [
+      "${pkgs.iproute2}/bin/ip rule del from 10.133.35.0/24 table 913335"
+      "${pkgs.iproute2}/bin/ip -6 rule del from 133:35::/64 table 913335"
+    ];
+    peers = [{
+      endpoint = "engage.cloudflareclient.com:2408";
+      publicKey = "bmXOC+F1FxEMF9dyiK2H5/1SUtzH0JuVo51h2wPfgyo=";
+      allowedIPs = [ "0.0.0.0/0" "::/0" ];
+      persistentKeepalive = 15;
+    }];
+  };
+
 }
