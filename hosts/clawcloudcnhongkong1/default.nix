de9ddc0552920a25517452189929f7abbdf40d2d
imlonghao <git@imlonghao.com>
2024-12-24 22:34:11 +0800

    hosts: move idcwiki to claw

--- hosts/clawcloudcnhongkong1/default.nix
+++ hosts/clawcloudcnhongkong1/default.nix
@@ -1,14 +1,39 @@
-{ pkgs, profiles, ... }: {
+{ config, lib, profiles, self, ... }: {
   imports = [
+    ./dn42.nix
+    ./bird.nix
     ./hardware.nix
     profiles.mycore
     profiles.users.root
     profiles.exporter.node
     profiles.etherguard.edge
     profiles.rsshc
+    profiles.pingfinder
+    profiles.docker
+    profiles.bird-lg-go
   ];
 
-  networking = { dhcpcd.allowInterfaces = [ "ens5" ]; };
+  networking = {
+    interfaces = {
+      lo = {
+        ipv4.addresses = [
+          {
+            address = "172.22.68.0";
+            prefixLength = 32;
+          }
+          {
+            address = "172.22.68.3";
+            prefixLength = 32;
+          }
+        ];
+        ipv6.addresses = [{
+          address = "fd21:5c0c:9b7e:3::";
+          prefixLength = 64;
+        }];
+      };
+    };
+    dhcpcd.allowInterfaces = [ "ens5" ];
+  };
 
   # EtherGuard
   services.etherguard-edge = {
@@ -36,6 +61,50 @@
         }
       ];
     };
+  };
+
+  # Vector
+  services.vector = {
+    enable = true;
+    journaldAccess = true;
+    settings = {
+      sources = {
+        docker = {
+          type = "docker_logs";
+          include_containers = [ "caddy" ];
+        };
+      };
+      transforms = {
+        parse_json = {
+          type = "remap";
+          inputs = [ "docker" ];
+          source = ''
+            . = parse_json!(.message)
+            .hostname = get_hostname!()
+          '';
+        };
+      };
+      sinks = {
+        caddycdn = {
+          type = "http";
+          inputs = [ "parse_json" ];
+          uri = "https://o2.esd.cc/api/default/caddycdn/_json";
+          method = "post";
+          auth.strategy = "basic";
+          auth.user = "\${O2_USER-default}";
+          auth.password = "\${O2_PASSWORD-default}";
+          compression = "gzip";
+          encoding.codec = "json";
+          encoding.timestamp_format = "rfc3339";
+          healthcheck.enabled = false;
+        };
+      };
+    };
+  };
+  sops.secrets.openobserve = { sopsFile = "${self}/secrets/openobserve.yml"; };
+  systemd.services.vector.serviceConfig = {
+    EnvironmentFile = config.sops.secrets.openobserve.path;
+    DynamicUser = lib.mkForce false;
   };
 
 }
