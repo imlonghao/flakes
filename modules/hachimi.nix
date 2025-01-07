bfc7bc53d7c4cfc2f3fe064d9a2ff05a38379641
imlonghao <git@imlonghao.com>
2025-01-07 21:43:36 +0800

    init hachimi

---
+++ modules/hachimi.nix
@@ -0,0 +1,28 @@
+{ config, pkgs, lib, ... }:
+let cfg = config.services.hachimi;
+in {
+  options.services.hachimi = {
+    enable = lib.mkEnableOption "Hachimi Honeypot";
+    path = lib.mkOption {
+      type = lib.types.str;
+      description = "path to the config file";
+    };
+    postStart = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      description = "additional commands to run after startup";
+      default = [ ];
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    systemd.services.hachimi = {
+      serviceConfig = {
+        WorkingDirectory = "${pkgs.hachimi}/share/hachimi";
+        ExecStart = "${pkgs.hachimi}/bin/honeypo -configPath ${cfg.path}";
+        ExecStartPost = cfg.postStart;
+      };
+      wants = [ "network-online.target" ];
+      after = [ "network-online.target" ];
+      wantedBy = [ "multi-user.target" ];
+    };
+  };
+}
---
+++ profiles/hachimi/default.nix
@@ -0,0 +1,8 @@
+{ sops, self, config, ... }: {
+  sops.secrets.hachimi.sopsFile =
+    "${self}/hosts/${config.networking.hostName}/secrets.yml";
+  services.hachimi = {
+    enable = true;
+    path = "/run/secrets/hachimi";
+  };
+}
