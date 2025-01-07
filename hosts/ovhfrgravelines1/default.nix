3df588da8f4c8fda24e8bab3cf53348bbd43a89d
imlonghao <git@imlonghao.com>
2025-01-07 21:53:02 +0800

    host(ovh): fix string

--- hosts/ovhfrgravelines1/default.nix
+++ hosts/ovhfrgravelines1/default.nix
@@ -214,12 +214,12 @@ in {
   # Hachimi
   services.hachimi = {
     postStart = [
-      ${pkgs.iptables}/bin/iptables -t mangle -A PREROUTING -i eth0 -p tcp --dport 222 -j TPROXY --on-port 12345 --on-ip 127.0.0.1
-      ${pkgs.iptables}/bin/iptables -t mangle -A PREROUTING -i eth0 -p tcp --dport 2222 -j TPROXY --on-port 12345 --on-ip 127.0.0.1
+      "${pkgs.iptables}/bin/iptables -t mangle -A PREROUTING -i eth0 -p tcp --dport 222 -j TPROXY --on-port 12345 --on-ip 127.0.0.1"
+      "${pkgs.iptables}/bin/iptables -t mangle -A PREROUTING -i eth0 -p tcp --dport 2222 -j TPROXY --on-port 12345 --on-ip 127.0.0.1"
     ];
     postStop = [
-      ${pkgs.iptables}/bin/iptables -t mangle -D PREROUTING -i eth0 -p tcp --dport 222 -j TPROXY --on-port 12345 --on-ip 127.0.0.1
-      ${pkgs.iptables}/bin/iptables -t mangle -D PREROUTING -i eth0 -p tcp --dport 2222 -j TPROXY --on-port 12345 --on-ip 127.0.0.1
+      "${pkgs.iptables}/bin/iptables -t mangle -D PREROUTING -i eth0 -p tcp --dport 222 -j TPROXY --on-port 12345 --on-ip 127.0.0.1"
+      "${pkgs.iptables}/bin/iptables -t mangle -D PREROUTING -i eth0 -p tcp --dport 2222 -j TPROXY --on-port 12345 --on-ip 127.0.0.1"
     ];
   };
 }
