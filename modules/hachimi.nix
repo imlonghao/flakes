cc78e2b27a0efa60862b8a942a71c90e1514a3b0
imlonghao <git@imlonghao.com>
2025-01-07 21:46:12 +0800

    module(hachimi): add postStop

--- modules/hachimi.nix
+++ modules/hachimi.nix
@@ -12,6 +12,11 @@ in {
       description = "additional commands to run after startup";
       default = [ ];
     };
+    postStop = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      description = "additional commands to run after shutdown";
+      default = [ ];
+    };
   };
   config = lib.mkIf cfg.enable {
     systemd.services.hachimi = {
@@ -19,6 +24,7 @@ in {
         WorkingDirectory = "${pkgs.hachimi}/share/hachimi";
         ExecStart = "${pkgs.hachimi}/bin/honeypo -configPath ${cfg.path}";
         ExecStartPost = cfg.postStart;
+        ExecStopPost = cfg.postStop;
       };
       wants = [ "network-online.target" ];
       after = [ "network-online.target" ];
