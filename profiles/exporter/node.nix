0a3688fd0f6525b12a54d42f2695297dd9d5fdb6
imlonghao <git@imlonghao.com>
2025-03-10 21:25:49 +0800

    profile(node_exporter): ignore swan

--- profiles/exporter/node.nix
+++ profiles/exporter/node.nix
@@ -8,8 +8,8 @@ in {
     listenAddress = ip;
     extraFlags = [
       "--collector.filesystem.ignored-mount-points=^/(dev|proc|sys|run|var/lib/docker/.+|var/lib/kubelet/.+|root/.+)($|/)"
-      "--collector.netclass.ignored-devices=^(docker[0-9]|vboxnet[0-9]|br-.+|veth.+)$"
-      "--collector.netdev.device-exclude=^(docker[0-9]|vboxnet[0-9]|br-.+|veth.+)$"
+      "--collector.netclass.ignored-devices=^(docker[0-9]|vboxnet[0-9]|br-.+|veth.+|swan.+)$"
+      "--collector.netdev.device-exclude=^(docker[0-9]|vboxnet[0-9]|br-.+|veth.+|swan.+)$"
     ];
   };
   systemd.services."prometheus-node-exporter".after =
