ef4424a3550165854d3e684f9b2f7378a44aaa82
imlonghao <git@imlonghao.com>
2025-05-16 21:31:58 +0800

    pkg(supervxlan): bump

--- pkgs/supervxlan/default.nix
+++ pkgs/supervxlan/default.nix
@@ -2,13 +2,13 @@
 
 buildGoModule rec {
   pname = "supervxlan";
-  version = "3e711925a1789543b8e3078c0e2795dbd45c9ee0";
+  version = "c75b6e054baef3cd7e626d9c82a736bb61cea1fe";
   src = fetchFromGitea {
     domain = "git.esd.cc";
     owner = "imlonghao";
     repo = pname;
     rev = version;
-    hash = "sha256-K8X+WTl2JWVbkwpR4riDmxMWsURL90vY/k92BibETLo=";
+    hash = "sha256-m2e41dxOQZDWWQRWj1pZ8GMJnzmmPVNBQ8xw9kt8Clc=";
   };
   vendorHash = "sha256-fJp906uP6TofwLddhv7jLQCsc1U1PUN53S9pbrPtp0Y=";
 
