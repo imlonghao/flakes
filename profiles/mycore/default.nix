eec6b624641a156ff6904a11630a9184b7e72c33
imlonghao <git@imlonghao.com>
2025-06-21 13:14:56 +0800

    profile(mycore): update sysctl

--- profiles/mycore/default.nix
+++ profiles/mycore/default.nix
@@ -23,8 +23,6 @@ in {
 
   boot.tmp.cleanOnBoot = true;
   boot.kernel.sysctl = {
-    "net.ipv4.tcp_congestion_control" = "bbr";
-    "net.ipv4.tcp_fastopen" = 3;
     "net.ipv4.conf.all.forwarding" = 1;
     "net.ipv6.conf.all.forwarding" = 1;
     "net.ipv4.conf.all.rp_filter" = 0;
@@ -39,11 +37,37 @@ in {
     "vm.watermark_scale_factor" = 125;
     "vm.page-cluster" = 0;
     # https://blog.cloudflare.com/optimizing-tcp-for-high-throughput-and-low-latency/
-    "net.ipv4.tcp_rmem" = "8192 262144 536870912";
-    "net.ipv4.tcp_wmem" = "4096 16384 536870912";
     "net.ipv4.tcp_adv_win_scale" = "-2";
     "net.ipv4.tcp_collapse_max_bytes" = 6291456;
     "net.ipv4.tcp_notsent_lowat" = 131072;
+    # https://wiki.archlinux.org/title/Sysctl
+    "net.core.netdev_max_backlog" = 16384;
+    "net.core.somaxconn" = 8192;
+    "net.core.rmem_default" = 1048576;
+    "net.core.rmem_max" = 16777216;
+    "net.core.wmem_default" = 1048576;
+    "net.core.wmem_max" = 16777216;
+    "net.core.optmem_max" = 65536;
+    "net.ipv4.tcp_rmem" = "4096 1048576 2097152";
+    "net.ipv4.tcp_wmem" = "4096 65536 16777216";
+    "net.ipv4.udp_rmem_min" = 8192;
+    "net.ipv4.udp_wmem_min" = 8192;
+    "net.ipv4.tcp_fastopen" = 3;
+    "net.ipv4.tcp_max_syn_backlog" = 8192;
+    "net.ipv4.tcp_max_tw_buckets" = 2000000;
+    "net.ipv4.tcp_tw_reuse" = 1;
+    "net.ipv4.tcp_fin_timeout" = 10;
+    "net.ipv4.tcp_slow_start_after_idle" = 0;
+    "net.ipv4.tcp_keepalive_time" = 60;
+    "net.ipv4.tcp_keepalive_intvl" = 10;
+    "net.ipv4.tcp_keepalive_probes" = 6;
+    "net.ipv4.tcp_mtu_probing" = 1;
+    "net.ipv4.tcp_timestamps" = 0;
+    "net.ipv4.tcp_sack" = 1;
+    "net.core.default_qdisc" = "cake";
+    "net.ipv4.tcp_congestion_control" = "bbr";
+    "net.ipv4.tcp_syncookies" = 1;
+    "net.ipv4.tcp_rfc1337" = 1;
   };
 
   environment.systemPackages = [ pkgs.bottom pkgs.mtr pkgs.tcpdump pkgs.wget ];
