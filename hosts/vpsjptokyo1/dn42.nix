8dce01e8163f6eaec30742cb1c9e0c9cd44eb1da
imlonghao <git@imlonghao.com>
2025-06-24 23:36:19 +0800

    host(jp): peer 4242420088

--- hosts/vpsjptokyo1/dn42.nix
+++ hosts/vpsjptokyo1/dn42.nix
@@ -4,6 +4,14 @@
   sops.secrets.wireguard.sopsFile = ./secrets.yml;
   dn42 = [
     {
+      name = "wg0088";
+      asn = 4242420088;
+      e6 = "fe80::88:3";
+      listen = 20088;
+      endpoint = "jp01-peer.furry.lv:21888";
+      publickey = "1fBfqOJnkf9blffCy5/DfQm+Sea3BkKikxUwOJA8CAs=";
+    }
+    {
       name = "wg2189";
       asn = 4242422189;
       e6 = "fe80::2189:ee";
