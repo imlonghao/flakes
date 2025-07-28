825ab3bf48f5c9a2418027539edd42d17d14aa29
imlonghao <git@imlonghao.com>
2025-07-28 10:44:04 +0800

    module(k3s): allow IPv6 INVALID packet

--- modules/k3s-no-ctstate-invalid.nix
+++ modules/k3s-no-ctstate-invalid.nix
@@ -16,7 +16,8 @@ in
     systemd.services.k3s-no-ctstate-invalid = {
       serviceConfig = {
         Type = "oneshot";
-        ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.iptables}/bin/iptables -w -L FORWARD 1 | ${pkgs.gnugrep}/bin/fgrep \"ctstate INVALID\" || ${pkgs.iptables}/bin/iptables -w -I FORWARD -m conntrack --ctstate INVALID -j ACCEPT'";
+        ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.iptables}/bin/iptables -w -C FORWARD -m conntrack --ctstate INVALID -j ACCEPT || ${pkgs.iptables}/bin/iptables -w -I FORWARD -m conntrack --ctstate INVALID -j ACCEPT'";
+        ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.iptables}/bin/ip6tables -w -C FORWARD -m conntrack --ctstate INVALID -j ACCEPT || ${pkgs.iptables}/bin/ip6tables -w -I FORWARD -m conntrack --ctstate INVALID -j ACCEPT'";
       };
       wants = [ "network-online.target" ];
       after = [ "network-online.target" ];
