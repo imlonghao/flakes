bfa82ad1410de3dd19aaeae45edceb543cad5517
imlonghao <git@imlonghao.com>
2025-07-31 21:50:54 +0800

    pkg(komari-agent): init at 0.1.1

---
+++ pkgs/komari-agent/package.nix
@@ -0,0 +1,32 @@
+{
+  lib,
+  fetchFromGitHub,
+  buildGoModule,
+  pkgs,
+}:
+buildGoModule rec {
+  pname = "komari-agent";
+  version = "0.1.1";
+
+  src = fetchFromGitHub {
+    owner = "komari-monitor";
+    repo = pname;
+    rev = version;
+    hash = "sha256-AllLRP5GrZKrt8qLnMjOsMXrSTND/uggAVPnKYLtp9w=";
+  };
+
+  vendorHash = "sha256-2tCYq7+r2VCQTULEt7gkK3ocz3YSoKi9yZ7NCVUTiH8=";
+
+  ldflags = [
+    "-s -w"
+    "-X=github.com/komari-monitor/komari-agent/update.CurrentVersion=${version}"
+  ];
+
+  doCheck = false;
+
+  meta = with lib; {
+    homepage = "https://github.com/komari-monitor/komari-agent";
+    description = "A lightweight server probe for simple, efficient monitoring";
+    license = licenses.mit;
+  };
+}
