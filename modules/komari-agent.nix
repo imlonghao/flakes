e71c9051106156a748a1362ae87df8bb67153ff4
imlonghao <git@imlonghao.com>
2025-08-01 21:05:31 +0800

    module(komari-agent): make service secured

--- modules/komari-agent.nix
+++ modules/komari-agent.nix
@@ -32,6 +32,24 @@ in
     systemd.services.komari-agent = {
       serviceConfig = {
         ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.komari-agent}/bin/komari-agent --disable-auto-update --disable-web-ssh -e ${cfg.endpoint} -t $(cat ${cfg.token}) --month-rotate ${toString cfg.month-rotate} --include-mountpoint \"${lib.strings.concatStringsSep ";" cfg.include-mountpoint}\"'";
+        Restart = "always";
+        DynamicUser = "yes";
+        ProtectSystem = "strict";
+        ProtectHome = "true";
+        PrivateDevices = "true";
+        PrivateMounts = "true";
+        ProtectKernelTunables = "true";
+        ProtectKernelModules = "true";
+        ProtectKernelLogs = "true";
+        ProtectControlGroups = "true";
+        LockPersonality = "true";
+        RestrictRealtime = "true";
+        ProtectClock = "true";
+        MemoryDenyWriteExecute = "true";
+        RestrictAddressFamilies = "AF_INET AF_INET6";
+        SocketBindDeny = "any";
+        CapabilityBoundingSet = "~CAP_BLOCK_SUSPEND CAP_BPF CAP_CHOWN CAP_MKNOD CAP_NET_RAW CAP_PERFMON CAP_SYS_BOOT CAP_SYS_CHROOT CAP_SYS_MODULE CAP_SYS_PACCT CAP_SYS_PTRACE CAP_SYS_TIME CAP_SYS_TTY_CONFIG CAP_SYSLOG CAP_WAKE_ALARM";
+        SystemCallFilter = "~@aio:EPERM @chown:EPERM @clock:EPERM @cpu-emulation:EPERM @debug:EPERM @keyring:EPERM @memlock:EPERM @module:EPERM @mount:EPERM @obsolete:EPERM @pkey:EPERM @privileged:EPERM @raw-io:EPERM @reboot:EPERM @sandbox:EPERM @setuid:EPERM @swap:EPERM @sync:EPERM @timer:EPERM";
       };
       wants = [ "network-online.target" ];
       after = [ "network-online.target" ];
