6a6bc890ecf7a587f080513f06b5b090a530d62f
imlonghao <git@imlonghao.com>
2025-11-25 23:07:39 +0800

    module(k3s): update script

--- modules/k3s-no-ctstate-invalid.nix
+++ modules/k3s-no-ctstate-invalid.nix
@@ -17,8 +17,8 @@ in
       serviceConfig = {
         Type = "oneshot";
         ExecStart = [
-          "-${pkgs.bash}/bin/bash -c '${pkgs.iptables}/bin/iptables -S FORWARD | head -2 | grep -F -- \"-A FORWARD -m conntrack --ctstate INVALID -j ACCEPT\" || (${pkgs.iptables}/bin/iptables -w -D FORWARD -m conntrack --ctstate INVALID -j ACCEPT;${pkgs.iptables}/bin/iptables -w -I FORWARD -m conntrack --ctstate INVALID -j ACCEPT)'"
-          "-${pkgs.bash}/bin/bash -c '${pkgs.iptables}/bin/ip6tables -S FORWARD | head -2 | grep -F -- \"-A FORWARD -m conntrack --ctstate INVALID -j ACCEPT\" || (${pkgs.iptables}/bin/ip6tables -w -D FORWARD -m conntrack --ctstate INVALID -j ACCEPT;${pkgs.iptables}/bin/ip6tables -w -I FORWARD -m conntrack --ctstate INVALID -j ACCEPT)'"
+          "-${pkgs.bash}/bin/bash -c '${pkgs.iptables}/bin/iptables -S FORWARD | head -3 | grep -F -- \"-A FORWARD -m conntrack --ctstate INVALID -j ACCEPT\" || (${pkgs.iptables}/bin/iptables -w -D FORWARD -m conntrack --ctstate INVALID -j ACCEPT;${pkgs.iptables}/bin/iptables -w -I FORWARD -m conntrack --ctstate INVALID -j ACCEPT)'"
+          "-${pkgs.bash}/bin/bash -c '${pkgs.iptables}/bin/ip6tables -S FORWARD | head -3 | grep -F -- \"-A FORWARD -m conntrack --ctstate INVALID -j ACCEPT\" || (${pkgs.iptables}/bin/ip6tables -w -D FORWARD -m conntrack --ctstate INVALID -j ACCEPT;${pkgs.iptables}/bin/ip6tables -w -I FORWARD -m conntrack --ctstate INVALID -j ACCEPT)'"
         ];
       };
       wants = [ "network-online.target" ];
