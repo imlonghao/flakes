974a6a6d7a7abb08a8e6ceafd2dddfb33f1d84d3
imlonghao <git@imlonghao.com>
2026-03-07 13:55:37 +0800

    container(act): use docket.sock

--- containers/act-runner.nix
+++ containers/act-runner.nix
@@ -6,14 +6,16 @@
   };
   virtualisation.oci-containers.containers = {
     act-runner = {
-      image = "vegardit/gitea-act-runner:dind-rootless-0.2.13";
-      privileged = true;
+      image = "vegardit/gitea-act-runner:0.2.13";
       environment = {
         GITEA_INSTANCE_URL = "https://git.esd.cc";
         GITEA_RUNNER_NAME = config.networking.hostName;
       };
       environmentFiles = [ config.sops.secrets.act-runner.path ];
-      volumes = [ "/persist/docker/act_runner/data:/data" ];
+      volumes = [
+        "/persist/docker/act_runner/data:/data"
+        "/run/podman/podman.sock:/var/run/docker.sock"
+      ];
     };
   };
 }
