a0aac28ed149635cb772bca3845d904a15cbdc5c
imlonghao <git@imlonghao.com>
2026-04-02 23:46:17 +0800

    pkg(supervxlan): update

--- pkgs/supervxlan/package.nix
+++ pkgs/supervxlan/package.nix
@@ -6,15 +6,15 @@
 
 buildGoModule rec {
   pname = "supervxlan";
-  version = "c75b6e054baef3cd7e626d9c82a736bb61cea1fe";
+  version = "1c05080913eb681fb09a31878a0539c05e9a75a2";
   src = fetchFromGitea {
     domain = "git.esd.cc";
     owner = "imlonghao";
     repo = pname;
     rev = version;
-    hash = "sha256-m2e41dxOQZDWWQRWj1pZ8GMJnzmmPVNBQ8xw9kt8Clc=";
+    hash = "sha256-neoeeWk0WUxJA66VVR19WW4xmu+zD/BqxieAiiTjZdc=";
   };
-  vendorHash = "sha256-fJp906uP6TofwLddhv7jLQCsc1U1PUN53S9pbrPtp0Y=";
+  vendorHash = "sha256-mhztklDaOsqaVtD2l2uS5AOGLl1sW4YF5/FTuIq2E14=";
 
   subPackages = [ "cmd/agent" ];
 
