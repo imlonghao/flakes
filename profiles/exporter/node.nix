1b034afafa3510bde2bde248674d5c92405cf6dd
imlonghao <git@imlonghao.com>
2026-05-26 21:52:38 +0800

    profile(node-exporter): ignore mount and device

--- profiles/exporter/node.nix
+++ profiles/exporter/node.nix
@@ -7,9 +7,9 @@ in
     enable = true;
     listenAddress = ip;
     extraFlags = [
-      "--collector.filesystem.ignored-mount-points=^/(dev|proc|sys|run|var/lib/docker/.+|var/lib/kubelet/.+|root/.+)($|/)"
-      "--collector.netclass.ignored-devices=^(docker[0-9]|vboxnet[0-9]|br-.+|veth.+|swan.+)$"
-      "--collector.netdev.device-exclude=^(docker[0-9]|vboxnet[0-9]|br-.+|veth.+|swan.+)$"
+      "--collector.filesystem.ignored-mount-points=^/(dev|proc|sys|run|var/lib/docker/.+|var/lib/kubelet/.+|root/.+|etc/.+|nix/store)($|/)"
+      "--collector.netclass.ignored-devices=^(docker[0-9]|vboxnet[0-9]|br-.+|veth.+|swan.+|dummy|podman[0-9])$"
+      "--collector.netdev.device-exclude=^(docker[0-9]|vboxnet[0-9]|br-.+|veth.+|swan.+|dummy|podman[0-9])$"
     ];
   };
   systemd.services."prometheus-node-exporter".after = [ "ranet.service" ];
