6562ae8b844c3c29163f61f78a3e39043db75c65
imlonghao <git@imlonghao.com>
2026-07-31 15:18:51 +0800

    pingfinder: remove legacy client

--- profiles/pingfinder/default.nix
+++
@@ -1,9 +0,0 @@
-{ config, self, ... }:
-
-{
-  sops.secrets.pingfinder.sopsFile = "${self}/hosts/${config.nixpkgs.system}/${config.networking.hostName}/secrets.yml";
-  services.pingfinder = {
-    enable = true;
-    environmentFile = config.sops.secrets.pingfinder.path;
-  };
-}
